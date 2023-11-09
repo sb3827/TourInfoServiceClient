6809724141b728882392de2c7ecd51575c2f0a12
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-10 01:22:26 +0900

    배경색 변경

--- src/components/manager/SubBox.tsx
+++ src/components/manager/SubBox.tsx
@@ -8,7 +8,7 @@ export const SubBox: FC<React.PropsWithChildren<SubBoxProps>> = ({children}) => 
   return (
     <div className="flex justify-center w-full mb-5">
       <div className="flex flex-col w-2/3 ">
-        <div className="p-3 overflow-y-auto border border-gray-300 rounded-lg max-h-96">
+        <div className="p-3 overflow-y-auto bg-white border border-gray-300 rounded-lg max-h-96">
           {children}
         </div>
       </div>
