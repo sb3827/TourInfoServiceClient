2862dc3f7b557dca81dfba54735917b10de9b576
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-10 01:22:56 +0900

    styles추가하여 스타일 변경 가능하게 하였음

--- src/components/manager/SubTitle.tsx
+++ src/components/manager/SubTitle.tsx
@@ -1,15 +1,17 @@
-import React, {FC} from 'react'
+import React, {FC, PropsWithChildren} from 'react'
 
 //박스 위의 글 스타일
-type SubTitleProps = {text: string}
+type SubTitleProps = {text: string; styles?: string}
 
 export const SubTitle: FC<React.PropsWithChildren<SubTitleProps>> = ({
   children,
-  text
+  text,
+  styles
 }) => {
+  const buttonClass = `text-xl font-bold text-left ${styles}`
   return (
     <div className="flex items-center p-3 ">
-      <h2 className="text-xl font-bold text-left">{text}</h2>
+      <h2 className={buttonClass}>{text}</h2>
       {children}
     </div>
   )
