f5048cc04986a820f679ff774beee51e7ad8a6da
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-10 01:45:58 +0900

    X를 아이콘으로 변경

--- src/components/manager/ReportList/ReportModal.tsx
+++ src/components/manager/ReportList/ReportModal.tsx
@@ -1,3 +1,5 @@
+import {faXmark} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import React, {FC} from 'react'
 
 type ReportModalProps = {
@@ -13,10 +15,12 @@ export const ReportModal: FC<React.PropsWithChildren<ReportModalProps>> = ({
   if (!isOpen) return null
 
   return (
-    <div className="fixed inset-0 z-50 flex overflow-auto bg-gray-600 bg-opacity-50 ">
-      <div className="relative flex flex-col w-full max-w-2xl p-8 m-auto bg-white rounded-xl ">
+    <div className="fixed inset-0 z-50 flex overflow-auto bg-gray-600 bg-opacity-50">
+      <div className="relative flex flex-col w-full max-w-2xl p-8 pl-10 pr-10 m-auto bg-white rounded-xl">
         <span className="absolute top-0 right-0 p-4" onClick={onClose}>
-          <button>X</button>
+          <button>
+            <FontAwesomeIcon icon={faXmark} size="2xl" />
+          </button>
         </span>
         {children}
       </div>
