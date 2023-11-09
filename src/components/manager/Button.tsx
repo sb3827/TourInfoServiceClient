3dd2b67a6192ed5a9621f486cb8e5e380f5d5840
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-10 01:22:15 +0900

    bgColor를 styles로 변경하여 버튼 스타일 추가능하도록 변경 및 div제거

--- src/components/manager/Button.tsx
+++ src/components/manager/Button.tsx
@@ -1,21 +1,19 @@
-import React, {FC} from 'react'
+import React, {FC, PropsWithChildren} from 'react'
 
 //관리자 페이지 버튼 스타일 - 추후 props(아마 대기목록의 회원 아이디) 더 추가해야함
 
 type ButtonProps = {
-  text?: string
-  bgColor?: string
+  text?: string & PropsWithChildren
+  styles?: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-export const Button: FC<ButtonProps> = ({text, bgColor, onClick}) => {
-  const buttonClass = `btn m-1 p-5 ${bgColor} border rounded-lg text-white`
+export const Button: FC<ButtonProps> = ({text, styles, onClick}) => {
+  const buttonClass = `btn m-1 p-5 ${styles} border rounded-lg text-white`
 
   return (
-    <div className="flex">
-      <button className={buttonClass} onClick={onClick}>
-        {text}
-      </button>
-    </div>
+    <button className={buttonClass} onClick={onClick}>
+      {text}
+    </button>
   )
 }
