6449516193d27fe91c7e67a639deebe7713a1ebc
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-10 01:20:38 +0900

    axios 기본 주소 숨김

--- src/api/Business/axios.ts
+++ src/api/Business/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 //사업자 확인 axios 기본 셋팅
 
 export const businesAxios = axios.create({
-  baseURL: 'https://api.odcloud.kr/api/nts-businessman/v1',
+  baseURL: process.env.REACT_APP_BUSINESS_ADDRESS,
   headers: {
     'Content-Type': 'application/json'
   },
