38c47ba0bfc2b350c864241ed3d52cc55067a13c
sb3827 <mmk275@naver.com>
2023-11-11 21:58:28 +0900

    add input, button

--- src/pages/Reply/Reply.tsx
+++ src/pages/Reply/Reply.tsx
@@ -1,11 +1,18 @@
 import {FC, PropsWithChildren} from 'react'
+import {Input, Button} from '../../components'
 
 type ReplyProps = {}
 
 export const Reply: FC<PropsWithChildren<ReplyProps>> = () => {
     return (
         <div>
-            input 넣고, 버튼 넣고
+            <div className="flex flex-row justify-center">
+                <Input
+                    placeholder="댓글을 작성해 주세요"
+                    className="mx-2 border-black"
+                    size={80}></Input>
+                <Button text="작성" styles="" />
+            </div>
             <div>reply list</div>
         </div>
     )
