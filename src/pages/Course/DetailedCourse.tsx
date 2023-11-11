f713ea3e8746211a4b26d3d489a0c9cca8b7d685
sb3827 <mmk275@naver.com>
2023-11-11 21:57:09 +0900

    add detailed course page structure

---
+++ src/pages/Course/DetailedCourse.tsx
@@ -0,0 +1,7 @@
+import {FC, PropsWithChildren} from 'react'
+
+type DetailedCourseType = {}
+
+export const DetailedCourse: FC<PropsWithChildren<DetailedCourseType>> = () => {
+    return <div></div>
+}
