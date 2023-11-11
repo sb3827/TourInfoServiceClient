0dc63312c6723673078edd7f6be723654d0126e4
sb3827 <mmk275@naver.com>
2023-11-11 21:58:09 +0900

    add map component

--- src/pages/Place/PostPlace.tsx
+++ src/pages/Place/PostPlace.tsx
@@ -1,5 +1,5 @@
 import {FC, PropsWithChildren} from 'react'
-import {Title} from '../../components'
+import {Title, Map} from '../../components'
 import {Reply} from '../Reply'
 
 type PostPlaceProps = {
@@ -9,21 +9,24 @@ type PostPlaceProps = {
 export const PostPlace: FC<PropsWithChildren<PostPlaceProps>> = ({title}) => {
     return (
         <div>
-            <div>
+            <div className="my-2">
                 {/*header*/}
                 <div>뒤로가기 버튼 변경 요망</div>
                 <Title>{title}</Title>
-                <div className="flex flex-row">
+                <div className="flex flex-row justify-end">
                     <div>별점</div>
                     <div>좋아요</div>
                 </div>
             </div>
-            <div>
+            <div className="my-2">
                 {/*body*/}
-                <div>img map</div>
+                <div className="flex flex-row justify-center">
+                    img
+                    <Map width="600px" height="400px"></Map>
+                </div>
                 <div>글....</div>
             </div>
-            <div>
+            <div className="my-2">
                 {/*footer*/}
                 <Reply />
             </div>
