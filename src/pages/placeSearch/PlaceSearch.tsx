d0019db3f1cdfd366388e72cea434bb8a52c2cea
honghyibeom <ghdgmlqja1@naver.com>
2023-11-13 19:52:49 +0900

    지도 추가

--- src/pages/placeSearch/PlaceSearch.tsx
+++ src/pages/placeSearch/PlaceSearch.tsx
@@ -1,13 +1,11 @@
 import React, {FC, useState} from 'react'
-import {Box, SearchInput, SubTitle, SubBox, FindBox} from '../../components/index'
-
-type FindBoxProps = {}
+import {Box, SearchInput, SubTitle, Map} from '../../components/index'
 
 // 장소검색 페이지
 
-type ManagerProps = {}
+type PlaceSearchProps = {}
 
-export const PlaceSearch: FC<ManagerProps> = ({}) => {
+export const PlaceSearch: FC<PlaceSearchProps> = ({}) => {
     //검색 값
     const [searchValue, setSearchValue] = useState<string>('')
 
@@ -16,27 +14,29 @@ export const PlaceSearch: FC<ManagerProps> = ({}) => {
         setSearchValue(value)
     }
 
-
-
     return (
         <Box>
             <div className="justify-center w-5/6 ">
                 <SubTitle text="장소 검색">
-                    <SearchInput value={searchValue} onChange={onChangeSearch} styles='flex' />
+                    <SearchInput
+                        value={searchValue}
+                        onChange={onChangeSearch}
+                        styles="flex"
+                    />
                 </SubTitle>
             </div>
-                <div className="flex justify-center w-full h-screen mb-32">
-                    <div className="flex w-5/6">
-                        <div className="w-2/6 p-3 overflow-y-auto border rounded-lg border--300">
-                                {/* 검색 결과를 보여줄 컴포넌트 */}
-                                <h2>검색 결과를 보여줄 컴포넌트</h2>
-                        </div>
-                        <div className="w-4/6 p-3 bg-green-400 border border-gray-300 rounded-lg">
-                             {/* MapAPI 컴포넌트 */}
-                             <h2>MapAPI 컴포넌트</h2>
-                        </div>
+            <div className="flex justify-center w-full h-screen mb-32">
+                <div className="flex w-5/6 h-5/6">
+                    <div className="w-2/6 p-3 overflow-y-auto border rounded-lg border--300">
+                        {/* 검색 결과를 보여줄 컴포넌트 */}
+                        <h2>검색 결과를 보여줄 컴포넌트</h2>
+                    </div>
+                    <div className="w-4/6 p-3 border border-gray-300 rounded-lg">
+                        {/* MapAPI 컴포넌트 */}
+                        <Map width="100%" height="100%"></Map>
                     </div>
                 </div>
+            </div>
         </Box>
     )
 }
