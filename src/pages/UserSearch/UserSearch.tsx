cb2f6280683753b538247c7ae997a68ffb5bc146
honghyibeom <ghdgmlqja1@naver.com>
2023-11-14 19:41:41 +0900

    유저검색 페이지 작성

---
+++ src/pages/UserSearch/UserSearch.tsx
@@ -0,0 +1,33 @@
+import {FC, PropsWithChildren, useState} from 'react'
+import {Box, SearchInput} from '../../components/index'
+import {SearchResult} from '../placeSearch/index'
+
+type UserSearchProps = {}
+
+export const UserSearch: FC<UserSearchProps> = () => {
+    //검색 값
+    const [searchValue, setSearchValue] = useState<string>('')
+
+    //입력때마다 검색값 업데이트
+    function onChangeSearch(value: string) {
+        setSearchValue(value)
+    }
+
+    return (
+        <Box>
+            <SearchInput
+                className="w-4/6 mb-4"
+                value={searchValue}
+                onChange={onChangeSearch}
+            />
+            <div className="flex justify-center w-full h-screen mb-32">
+                <div className="flex w-5/6 h-5/6">
+                    <div className="w-full p-3 overflow-y-auto border rounded-lg border--300">
+                        {/* 검색 결과를 보여줄 컴포넌트 */}
+                        <SearchResult text="프로필" />
+                    </div>
+                </div>
+            </div>
+        </Box>
+    )
+}
