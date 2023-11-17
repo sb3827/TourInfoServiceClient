16ac0d914f80059eb30d05567e66e79bc1949605
이해창 <gockd9968@naver.com>
2023-11-17 19:46:33 +0900

    마이페이지 작성글조회 버튼

---
+++ src/components/myPage/WritingButton.tsx
@@ -0,0 +1,51 @@
+import {FC, useState} from 'react'
+import {MyCourseBox} from './ShowMyWriting/MyCourseBox'
+import {MyPostBox} from './ShowMyWriting/MyPostBox'
+import {MyReplyBox} from './ShowMyWriting/MyReplyBox'
+
+// 마이페이지 작성 글 보기 버튼
+
+type WritingButtonProps = {}
+
+export const WritingButton: FC<WritingButtonProps> = ({}) => {
+    const [content, setContent] = useState(<MyPostBox />)
+    const [toggle, setToggle] = useState('post')
+
+    const components: {[key: string]: JSX.Element} = {
+        post: <MyPostBox />,
+        course: <MyCourseBox />,
+        reply: <MyReplyBox />
+    }
+
+    const onChangeContent = (type: string) => {
+        setContent(components[type])
+        setToggle(type)
+    }
+
+    return (
+        <div>
+            <button
+                onClick={() => onChangeContent('post')}
+                className={`w-40 h-12 ml-32 mb-8 border-2 rounded-md focus:outline-none focus:shadow-outline ${
+                    toggle === 'post' ? 'btn-info text-white' : 'btn-ghost'
+                }`}>
+                작성 게시글
+            </button>
+            <button
+                onClick={() => onChangeContent('course')}
+                className={`w-40 h-12 border-2 rounded-md focus:outline-none focus:shadow-outline ${
+                    toggle === 'course' ? 'btn-info text-white' : 'btn-ghost'
+                }`}>
+                여행 코스
+            </button>
+            <button
+                onClick={() => onChangeContent('reply')}
+                className={`w-40 h-12 border-2 rounded-md focus:outline-none focus:shadow-outline ${
+                    toggle === 'reply' ? 'btn-info text-white' : 'btn-ghost'
+                }`}>
+                댓글 목록
+            </button>
+            <div className="flex justify-center w-full">{content}</div>
+        </div>
+    )
+}
