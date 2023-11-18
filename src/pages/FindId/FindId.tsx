dea1578ada279a362ff2c659cc7da8e4cc351b77
yununyu <myamoly10@gmail.com>
2023-11-18 16:42:26 +0900

    Button -> ToggleButton 으로 수정

--- src/pages/FindId/FindId.tsx
+++ src/pages/FindId/FindId.tsx
@@ -1,58 +1,68 @@
-import React, {FC, useState} from 'react'
-import {Input, Button} from '../../components'
+import React, { FC, useState } from 'react';
+import { Input, ToggleButton ,Button,Subtitle} from '../../components';
 
-type FindIdProps = {}
+type FindIdProps = {};
 
 export const FindId: FC<FindIdProps> = () => {
-    const [nowActivatedTabValue, setNowActivatedTabValue] = useState('')
+    const [nowActivatedTabValue, setNowActivatedTabValue] = useState("FindId");
 
     return (
-        <div className="flex flex-col items-center max-w-md p-6 mx-auto bg-gray-400 rounded-lg shadow-md">
+        <div className="flex flex-col items-center max-w-md p-6 mx-auto bg-gray-300 rounded-lg shadow-md">
             <div className="mb-8 text-3xl font-bold text-center">Logo</div>
-            <Button
-                value="FindId"
-                onClick={() => {
-                    setNowActivatedTabValue('FindId')
-                }}></Button>
-            <Button
-                value="FindPassword"
-                onClick={() => {
-                    setNowActivatedTabValue('FindPassword')
-                }}></Button>
+            <ToggleButton >
+                <Subtitle 
+                    value="아이디 찾기"
+                    className="ml-5 text-left flex items-center flex-row-reverse"
+                    onClick={() => setNowActivatedTabValue("FindId")}> 
+                </Subtitle>
+                <Subtitle 
+                    value="비밀번호 찾기"
+                    className="ml-5 text-left flex items-center flex-row-reverse"
+                    onClick={() => setNowActivatedTabValue("FindPassword")}> 
+                </Subtitle>    
+            </ToggleButton>
             {nowActivatedTabValue === 'FindId' && (
                 <>
-                    <div className="flex flex-row items-center mb-4">
+                    <div className="flex flex-row items-center mb-4 mt-10">
                         <label htmlFor="Name" className="w-40">
                             이름을 입력하세요
                         </label>
                         <Input className="p-2 border rounded" id="Name" />
                     </div>
-                    <div className="flex flex-row items-center mb-4">
+                    <div className="flex flex-row items-center mb-8">
                         <label htmlFor="Email" className="w-40">
                             이메일을 입력하세요
                         </label>
                         <Input className="p-2 border rounded" id="Email" />
                     </div>
-                    <Button value="아이디 찾기"></Button>
+                    <Button 
+                        value="아이디 찾기"
+                        onClick={() => alert("아이디를 이메일로 전송하였습니다")}
+                        >
+                    </Button>
                 </>
             )}
             {nowActivatedTabValue === 'FindPassword' && (
                 <>
-                    <div className="flex flex-row items-center mb-4">
+                    <div className="flex flex-row items-center mb-4 mt-10">
                         <label htmlFor="Id" className="w-40">
                             아이디를 입력하세요
                         </label>
                         <Input className="p-2 border rounded" id="Id" />
                     </div>
-                    <div className="flex flex-row items-center mb-4">
+                    <div className="flex flex-row items-center mb-8">
                         <label htmlFor="Email" className="w-40">
                             이메일을 입력하세요
                         </label>
                         <Input className="p-2 border rounded" id="Email" />
                     </div>
-                    <Button value="비밀번호 찾기"></Button>
+                    <Button 
+                        value="비밀번호 찾기"
+                        onClick={() => alert("비밀번호를 이메일로 전송하였습니다")}
+                        >
+                    </Button>
                 </>
             )}
         </div>
-    )
-}
+    );
+};
