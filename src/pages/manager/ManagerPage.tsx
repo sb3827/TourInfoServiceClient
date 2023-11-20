9096667398c7e81aecddf48844ca76be1560f4b8
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-20 19:06:47 +0900

    파일명 수정 Manager->ManagerPage

---
+++ src/pages/manager/ManagerPage.tsx
@@ -0,0 +1,57 @@
+import {FC, useState} from 'react'
+import {
+    Box,
+    FindBox,
+    ReportBox,
+    SearchInput,
+    Sidebar,
+    Subtitle,
+    ToggleButton,
+    WaitBox
+} from '../../components/index'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBell, faList} from '@fortawesome/free-solid-svg-icons'
+
+//관리자 페이지
+
+type ManagerProps = {}
+
+export const Manager: FC<ManagerProps> = ({}) => {
+    //검색 값
+    const [searchValue, setSearchValue] = useState<string>('')
+
+    //입력때마다 검색값 업데이트
+    function onChangeSearch(value: string) {
+        setSearchValue(value)
+    }
+
+    return (
+        <Box>
+            <ToggleButton>
+                <Subtitle
+                    value="회원대기 목록"
+                    className="ml-5 text-left flex items-center flex-row-reverse">
+                    <FontAwesomeIcon icon={faList} className="m-1" />
+                </Subtitle>
+                <Subtitle
+                    value="신고 목록"
+                    className="ml-5 text-left flex items-center flex-row-reverse">
+                    <FontAwesomeIcon icon={faBell} className="m-1" />
+                </Subtitle>
+                <WaitBox />
+                <ReportBox />
+            </ToggleButton>
+
+            <div className="w-2/3 ml-4">
+                <Subtitle value="사용자 검색" className="flex items-center m-5">
+                    <SearchInput
+                        className="w-1/2"
+                        value={searchValue}
+                        onChange={onChangeSearch}
+                    />
+                </Subtitle>
+            </div>
+            <FindBox />
+        </Box>
+    )
+}
