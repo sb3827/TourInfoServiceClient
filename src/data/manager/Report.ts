26423936d8763e393a1e52742195ea1934eedf63
myh98 <fldh3369@naver.com>
2023-11-23 01:24:21 +0900

    ReportData 오타 수정 및 reportDate 타입 Date로 수정, lno->lisetNum으로변경

--- src/data/manager/Report.ts
+++ src/data/manager/Report.ts
@@ -1,8 +1,8 @@
 //신고 정보
-export type RepoartData = {
-  reportDate: string //날짜
-  lno: string //게시글 번호
-  userId: string //아이디
-  reportedUser: string //신고 유저
-  reportDetail: string //신고사유
+export type ReportData = {
+    reportDate: Date //날짜
+    listNum: number //게시글 번호
+    userId: string //아이디
+    reportedUser: string //신고 유저
+    reportDetail: string //신고사유
 }
