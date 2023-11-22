44607c03c64875f8b4765f07b3b709d496385af7
honghyibeom <ghdgmlqja1@naver.com>
2023-11-22 18:47:07 +0900

    Slider 수정

--- src/components/place/Slider.tsx
+++ src/components/place/Slider.tsx
@@ -2,6 +2,7 @@
 
 // Import Swiper React components
 import {Swiper, SwiperSlide} from 'swiper/react'
+import React, {FC, PropsWithChildren} from 'react'
 
 // Import Swiper styles
 import 'swiper/css'
@@ -10,19 +11,18 @@ import 'swiper/css/navigation'
 // import required modules
 import {Navigation} from 'swiper/modules'
 
-export const Slider = () => {
+// children으로 전달된 요소들을 React.Children.map을 사용하여 반복,
+// children에 포함된 각 요소들을 배열로 변환
+
+export const Slider: FC<PropsWithChildren> = ({children}) => {
+    const slides = React.Children.map(children, (child, index) => (
+        <SwiperSlide key={index}>{child}</SwiperSlide>
+    ))
+
     return (
         <div className="flex justify-center w-full h-full border rounded-lg bg-slate-300 border--30">
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide></SwiperSlide>
-                <SwiperSlide></SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                {slides}
             </Swiper>
         </div>
     )
