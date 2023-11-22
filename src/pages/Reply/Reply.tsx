7423befd6d8a3a2f26cd0b7d73b6aee8d2b45e2e
sb3827 <mmk275@naver.com>
2023-11-22 23:09:56 +0900

    댓글 보드 댓글 컴포넌트 추가

--- src/pages/Reply/Reply.tsx
+++ src/pages/Reply/Reply.tsx
@@ -1,8 +1,10 @@
 import {FC, PropsWithChildren} from 'react'
-import {Input, Button} from '../../components'
+import {Input, Button, Comment} from '../../components'
+import {array} from "../../dummy data/sb's dummy"
 
 type ReplyProps = {}
 
+// 댓글 part 작성+list
 export const Reply: FC<PropsWithChildren<ReplyProps>> = () => {
     return (
         <div>
@@ -13,7 +15,13 @@ export const Reply: FC<PropsWithChildren<ReplyProps>> = () => {
                     size={80}></Input>
                 <Button text="작성" styles="" />
             </div>
-            <div>reply list</div>
+            <div className="flex justify-center">
+                <Comment
+                    img={array[0]}
+                    user={array[1]}
+                    comment={array[2]}
+                    date={array[3]}></Comment>
+            </div>
         </div>
     )
 }
