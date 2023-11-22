aa8eee123eb52cb44c49f99e4f35e5bba5fddff7
sb3827 <mmk275@naver.com>
2023-11-22 23:10:48 +0900

    장소 포스팅 페이지 작성

--- src/pages/Place/PostPlace.tsx
+++ src/pages/Place/PostPlace.tsx
@@ -1,21 +1,57 @@
-import {FC, PropsWithChildren} from 'react'
+import {FC, PropsWithChildren, useState} from 'react'
 import {Title, Map} from '../../components'
 import {Reply} from '../Reply'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHeart, faStar, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
 
 type PostPlaceProps = {
     title: string
 }
 
 export const PostPlace: FC<PropsWithChildren<PostPlaceProps>> = ({title}) => {
+    // heart button state
+    const [heart, setHeart] = useState<boolean>(false)
+    function clickHeart() {
+        setHeart(!heart)
+    }
     return (
         <div>
             <div className="my-2">
                 {/*header*/}
-                <div>뒤로가기 버튼 변경 요망</div>
+                <div className="flex justify-start mx-10 my-6">
+                    <FontAwesomeIcon icon={faArrowLeft} size="2xl" />
+                </div>
                 <Title>{title}</Title>
                 <div className="flex flex-row justify-end">
-                    <div>별점</div>
-                    <div>좋아요</div>
+                    <div>
+                        <FontAwesomeIcon
+                            icon={faStar}
+                            size="2xl"
+                            style={{color: '#fbfe3e'}}
+                        />
+                        {' 작성자별점'}
+                    </div>
+                    <div className="ml-4">
+                        {heart && (
+                            <FontAwesomeIcon
+                                className="hover:cursor-pointer"
+                                icon={faHeart}
+                                size="2xl"
+                                style={{color: '#ff3050'}}
+                                onClick={clickHeart}
+                            />
+                        )}
+                        {heart || (
+                            <FontAwesomeIcon
+                                className="hover:cursor-pointer"
+                                icon={faHeart}
+                                size="2xl"
+                                style={{color: '#c2c2c2'}}
+                                onClick={clickHeart}
+                            />
+                        )}
+                        {' 횟수'}
+                    </div>
                 </div>
             </div>
             <div className="my-2">
