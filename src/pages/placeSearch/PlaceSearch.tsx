f24cbf3a0f1946f4b4f7a090ef4a02060e09f557
honghyibeom <ghdgmlqja1@naver.com>
2023-11-22 18:49:28 +0900

    SearchInfo로 컴포넌트 이름 수정

--- src/pages/placeSearch/PlaceSearch.tsx
+++ src/pages/placeSearch/PlaceSearch.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useState} from 'react'
-import {Box, SearchInput, Map, SearchResult} from '../../components/index'
+import {Box, SearchInput, Map, SearchInfo} from '../../components/index'
 
 // 장소검색 페이지
 
@@ -14,13 +14,13 @@ export const PlaceSearch: FC<PlaceSearchProps> = ({}) => {
         setSearchValue(value)
     }
 
-   // 더미 검색 결과 데이터 (7개의 더미 데이터 생성)
-    const searchResults = Array.from({ length: 7 }, (_, index) => ({
+    // 더미 검색 결과 데이터 (7개의 더미 데이터 생성)
+    const searchResults = Array.from({length: 7}, (_, index) => ({
         name: `장소 ${index + 1}`,
         address: `주소`,
         information: `정보`,
-        imageUrl: `이미지 URL`, // 더미 이미지 URL로 대체 가능
-    }));
+        imageUrl: `이미지 URL` // 더미 이미지 URL로 대체 가능
+    }))
     // 더미 데이터
 
     return (
@@ -34,15 +34,15 @@ export const PlaceSearch: FC<PlaceSearchProps> = ({}) => {
                 <div className="flex w-5/6 h-5/6">
                     <div className="w-2/6 p-3 overflow-y-auto border rounded-lg border--300">
                         {/* 검색 결과를 보여줄 컴포넌트 */}
-                       {/* 더미 검색 결과에 대해 SearchResult 컴포넌트 여러 번 렌더링 */}
-                       {searchResults.map((result) => (
-                            <SearchResult
+                        {/* 더미 검색 결과에 대해 SearchResult 컴포넌트 여러 번 렌더링 */}
+                        {searchResults.map(result => (
+                            <SearchInfo
                                 name={result.name}
                                 address={result.address}
                                 information={result.information}
                                 imageUrl={result.imageUrl}
                             />
-                        ))}            
+                        ))}
                     </div>
                     <div className="w-4/6 p-3 border border-gray-300 rounded-lg">
                         {/* MapAPI 컴포넌트 */}
