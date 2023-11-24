a5be6e24782f5c02db1ec6a5465cc94617541474
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-24 16:39:28 +0900

    Box -> ClassName 추가

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,13 +1,10 @@
 import {FC, PropsWithChildren, HTMLAttributes} from 'react'
 
-type BoxProps = {}
+type BoxProps = {
+    className?: string
+}
 
-//FIXME - 영현 className을 줘서 외부에서 className으로 수정 시키도록 고쳐 줘
-
-export const Box: FC<PropsWithChildren<BoxProps>> = ({children}) => {
-    return (
-        <div className="flex flex-col items-center justify-center w-full bg-gray-100 pt-14">
-            {children}
-        </div>
-    )
+export const Box: FC<PropsWithChildren<BoxProps>> = ({children, className}) => {
+    const BoxStyle = `flex flex-col items-center justify-center w-full bg-gray-100 pt-14 ${className}`
+    return <div className={BoxStyle}>{children}</div>
 }
