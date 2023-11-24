6c8b42fae232a011906e49c6e02d6e8538be0ff4
honghyibeom <ghdgmlqja1@naver.com>
2023-11-24 17:15:04 +0900

    게시판, 썸네일 구분

--- src/components/place/Board.tsx
+++ src/components/place/Board.tsx
@@ -9,20 +9,31 @@ type BoardProps = {
 }
 
 export const Board: FC<BoardProps> = ({no, name, id, date, imageUrl}) => {
-    return (
+    // 썸네일 이미지 렌더링 부분
+    const renderImage = imageUrl && <img src={imageUrl} alt="Board Image" />
+
+    // 게시판 내용 렌더링 부분
+    const renderBoardContent = (
         <div>
             {/* 게시글 더미데이터 */}
             <div className="flex items-center w-full border-b-2">
                 <div className="w-3/4 p-3 m-5 rounded-lg">
-                    <div className="flex justify-between w-full ">
+                    <div className="flex justify-between w-full">
                         <div>{no}</div>
                         <div>{name}</div>
                         <div>{id}</div>
                         <div>{date}</div>
-                        <div>{imageUrl}</div>
                     </div>
                 </div>
             </div>
         </div>
     )
+
+    // 썸네일 이미지와 게시판 내용을 반환
+    if (imageUrl) {
+        return <div>{renderImage}</div>
+    }
+
+    // 썸네일 이미지가 없는 경우에는 게시판 내용만 반환
+    return renderBoardContent
 }
