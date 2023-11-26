dc4df45ace07dceee3471996ce44c800d3f842e8
yununyu <myamoly10@gmail.com>
2023-11-26 13:47:15 +0900

    슬라이더 기능 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,63 +1,82 @@
-import {FC, PropsWithChildren} from 'react'
-import {SearchInput} from '../../components'
-import React,{useState} from 'react'
-import { Box } from '../../components'
-
-type MainProps = {}
+import { FC, useState, ReactNode } from 'react'
+import { SearchInput } from '../../components'
+import { Slider } from '../../components'
+import { SliderItem } from '../../components/place/SliderItem';
 
 
-export const Main: FC<PropsWithChildren<MainProps>> = () => {
-    
-    const [searchValue,setSearchValue] = useState('');
-    function onSearch(value: string) {
-        setSearchValue(value)
-        // 검색로직추가
-    }
+type MainProps = {};
+
+export const Main: FC<MainProps> = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  function onSearch(value: string) {
+    setSearchValue(value);
+    // 검색로직추가
+  }
 
 
 
-    return (
-        <div className="flex flex-col justify-center h-screen ">
-         <div className="mb-8 text-3xl font-bold text-center">Logo</div>
-         <div>
-            <SearchInput value='searchValue' className='mb-10' onChange={onSearch}></SearchInput>
-         </div>
-         <div>
-            <label className="m-4 text-left block">
-                MostLiked♥
-            </label>
-            <Box>MostLiked♥</Box>
-
-            <label  className="m-4 text-left block">
-                recently upload
-            </label>
-            <Box>recently upload</Box> 
-
-            <label className="m-4 text-left block">
-                most reply
-            </label>
-            <Box>most reply</Box> 
-
-            <label className="m-4 text-left block">
-                mostLikedCourse♥ 
-            </label>
-            <Box>mostLikedCourse♥</Box> 
-
-            <label className="m-4 text-left block">
-              follower posts
-            </label>
-            <Box>follower posts</Box>
-
-            <label  className="m-4 text-left block">
-                ads
-            </label>
-            <Box>ads</Box>
-
-            
-         </div>
+  return (
+    <div>
+      <div className="flex flex-col justify-center h-screen">
+        <div className="m-8 text-3xl font-bold text-center">Logo</div>
+        <div>
+          <SearchInput
+            value={searchValue}
+            className="mb-10"
+            onChange={onSearch}
+          ></SearchInput>
         </div>
-      );
-      
-      
-      
+        <div>
+          <label className="m-4 text-left block">MostLiked♥</label>
+          <Slider>
+            <SliderItem onClick={() => alert('상세페이지로 이동')}>
+              <div>dummyContent1</div>
+            </SliderItem>
+            <SliderItem onClick={() => alert('상세페이지로 이동')}>
+              <div>dummyContent2</div>
+            </SliderItem>
+          </Slider>
+
+          <label className="m-4 text-left block">
+            recently upload
+          </label>
+          <Slider>
+
+          </Slider>
+
+          <label className="m-4 text-left block">
+            most reply
+          </label>
+          <Slider>
+
+          </Slider>
+
+          <label className="m-4 text-left block">
+            mostLikedCourse♥
+          </label>
+          <Slider>
+
+          </Slider>
+
+          <label className="m-4 text-left block">
+            follower posts
+          </label>
+          <Slider>
+
+          </Slider>
+
+          <label className="m-4 text-left block">
+            ads
+          </label>
+          <Slider>
+
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+
+
+
 }
