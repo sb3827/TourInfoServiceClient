44e581a8653545807d661d027b04346797903fa6
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-28 00:51:05 +0900

    mdb5 css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import '@fontsource/material-icons'
+//MDB5 CSS 추가
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
