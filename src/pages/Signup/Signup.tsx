79fa2e71b77995d696a245b3ff9806bde7601555
yununyu <myamoly10@gmail.com>
2023-11-28 19:34:22 +0900

    ERD 에 맞춰서 회원가입 페이지 요소 수정

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,82 +1,76 @@
 import {FC, PropsWithChildren, useState} from 'react'
-import {Input, Button} from '../../components'
+import {Input, Button, DropdownSelect} from '../../components'
 
 type SignupProps = {}
 
-
-
 export const Signup: FC<PropsWithChildren<SignupProps>> = () => {
+    const [selectValue, setSelectValue] = useState<string>('naver.com')
 
-    const [phoneNumber, setPhoneNumber] = useState("010-");       // default input value 를 설정함
-    
+    function onChangeSelect(e: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectValue(e.target.value)
+    }
 
     return (
-        <div className="flex justify-center items-center h-screen">
-          <section className="w-full max-w-[800px] h-auto bg-gray-200 p-8 rounded-lg">
-            <div className="text-3xl font-bold mb-8 text-center">Logo</div>
-            <div className="text-2xl font-bold mb-8 text-center">Signup</div>
-            <div className="flex flex-col items-center space-y-4">
-              <div className="flex flex-row items-center ">
-                <label htmlFor="ID" className="w-24 ml-20 pl-12">
-                  아이디
-                </label>
-                <div>
-                  <Input className="p-2 border rounded ml-7" id="ID" />
-                  <Button value="중복확인" className="ml-2"></Button>
+        <div className="flex flex-col items-center justify-center h-screen ">
+            <section className="w-auto h-auto p-8 bg-gray-300 rounded-lg">
+                <div className="mb-8 text-3xl font-bold text-center">Logo</div>
+                <div className="flex flex-row ">
+                    <label htmlFor="ID" className="font-bold">
+                        이메일
+                    </label>
+                    <Input className="border rounded" id="Email" />
+                    <DropdownSelect>
+                        <select
+                            onChange={onChangeSelect}
+                            value={selectValue}
+                            className="block w-full py-3 pl-3 pr-10 leading-tight border border-gray-300 shadow appearance-none rounded-2xl focus:outline-none focus:shadow-outline">
+                            <option value="naver.com">naver.com</option>
+                            <option value="google.com">google.com</option>
+                        </select>
+                    </DropdownSelect>
+                    <Button value="중복확인" className="ml-2"></Button>
                 </div>
-              </div>
-              <div className="flex flex-row items-center mb-4">
-                <label htmlFor="Password" className="w-24">
-                  비밀번호
-                </label>
-                <Input className="p-2 border rounded" id="Password" />
-              </div>
-              <div className="flex flex-row items-center mb-4">
-                <label htmlFor="Password" className="w-24">
-                  비밀번호 재입력
-                </label>
-                <Input className="p-2 border rounded" id="Password" />
-              </div>
-              <div className="flex flex-row items-center mb-4">
-                <label htmlFor="Username" className="w-24">
-                  이름
-                </label>
-                <Input className="p-2 border rounded" id="Username" />
-              </div>
-              <div className="flex flex-row items-center mb-4">
-                <label htmlFor="Birthdate" className="w-24">
-                  생년월일
-                </label>
-                <Input className="p-2 border rounded" id="Birthdate" placeholder="생년월일 6자리"/>
-              </div>
-              <div className="flex flex-row items-center mb-4">
-                {/* 드랍다운 버튼 추가? */}
-                <label htmlFor="PhoneNumber" className="w-24">
-                  전화번호
-                </label>
-                <Input className="p-2 border rounded" id="PhoneNumber" value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)} />
-                {/* 인증기능? */}
-              </div>
-              <div className="flex flex-row items-center mb-4">
-                <label htmlFor="Email" className="w-24">
-                  이메일
-                </label>
-                <Input className="p-2 border rounded" id="Email" />
-              </div>
-              <div className="flex justify-center items-center">
-                <Button
-                  value="가입하기"
-                  onClick={() => console.log('Signup Button clicked')}
-                ></Button>
-                {/* 취소하기 버튼? */}
-              </div>
-            </div>
-          </section>
+                <div className="flex flex-row items-center mb-4">
+                    <label htmlFor="Password" className="w-24 font-bold">
+                        비밀번호
+                    </label>
+                    <Input className="p-2 border rounded" id="Password" />
+                </div>
+                <div className="flex flex-row items-center mb-4">
+                    <label htmlFor="Password" className="w-24 font-bold">
+                        비밀번호 재입력
+                    </label>
+                    <Input className="p-2 border rounded" id="Password" />
+                </div>
+                <div className="flex flex-row items-center mb-4">
+                    <label htmlFor="Username" className="w-24 font-bold">
+                        이름
+                    </label>
+                    <Input className="p-2 border rounded" id="Username" />
+                </div>
+                <div className="flex flex-row items-center mb-4">
+                    <label htmlFor="Birthdate" className="w-24 font-bold">
+                        생년월일
+                    </label>
+                    <Input
+                        className="p-2 border rounded"
+                        id="Birthdate"
+                        placeholder="생년월일 6자리"
+                    />
+                </div>
+                <div className="flex flex-row items-center mb-4">
+                    <label htmlFor="PhoneNumber" className="w-24 font-bold">
+                        휴대폰 번호
+                    </label>
+                    <Input className="border rounded " id="PhoneNumber1" />
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <Button
+                        value="가입하기"
+                        onClick={() => console.log('Signup Button clicked')}></Button>
+                </div>
+            </section>
         </div>
-      );
-      
-      
-      
+    )
 }
-
-
