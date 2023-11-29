30d52390f85d5160c389ab6b5c7b71978ecd5359
honghyibeom <ghdgmlqja1@naver.com>
2023-11-29 18:56:25 +0900

    daisy ui 추가

--- src/components/place/SearchInfo.tsx
+++ src/components/place/SearchInfo.tsx
@@ -1,4 +1,5 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
+import {Button} from '../index'
 
 type SearchResultProps = {
     name: string
@@ -15,17 +16,40 @@ export const SearchInfo: FC<SearchResultProps> = ({
     imageUrl,
     reviewCount
 }) => {
+    const [activeIndex, setActiveIndex] = useState<number | null>(0)
+
+    const handleAccordion = (index: number) => {
+        setActiveIndex(prevIndex => (prevIndex === index ? null : index))
+    }
     return (
-        <div className="flex w-full text-2xl border rounded-lg border--300 bg-slate-300">
-            <div className="w-1/2 h-44">
+        <div className="collapse bg-base-200">
+            <input
+                type="radio"
+                name="my-accordion-1"
+                checked={activeIndex === 0}
+                onChange={() => handleAccordion(0)}
+            />
+            <div
+                className="text-xl font-medium collapse-title"
+                onClick={() => handleAccordion(0)}>
+                <div className="w-full border-2 h-44">
+                    <img src={imageUrl} alt="Image" />
+                </div>
                 <div>{name}</div>
                 <div>{address}</div>
                 <div>{rating}</div>
                 <div>{reviewCount}</div>
             </div>
-            <div className="w-1/2 h-44">
-                <img src={imageUrl} alt="Image" />
-            </div>
+            {activeIndex === 0 && (
+                <div className="collapse-content">
+                    <div className="flex justify-end w-4/6">
+                        <Button
+                            value="리뷰 보러가기"
+                            className="bg-gradient-to-r bg-slate-500"
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/user/SearchUserInfo.tsx
+++ src/components/user/SearchUserInfo.tsx
@@ -1,26 +1,68 @@
 import React, {FC} from 'react'
 
 type SearchResultProps = {
+    name: string
     profileImage: string
-    information: string
-    id: string
+    followingCount: number
+    ReviewCount: number
+    email: string
 }
 
 export const SearchUserInfo: FC<SearchResultProps> = ({
+    name,
     profileImage,
-    information,
-    id
+    followingCount,
+    ReviewCount,
+    email
 }) => {
     return (
-        <div className="flex text-2xl border h-44 bg-slate-300">
-            <div className="avatar">
-                <div className="border-2 rounded-full ">
-                    <img src={profileImage} />
+        <div className="shadow stats">
+            <div className="stat">
+                <div className="stat-figure text-secondary">
+                    <img
+                        src={profileImage}
+                        alt="profileImage"
+                        className="w-16 rounded-full"
+                    />
                 </div>
+                <div className="stat-value">{name}</div>
+                <div className="stat-title">{email}</div>
             </div>
-            <div className="w-full h-full border-2">
-                <div>{information}</div>
-                <div>{id}</div>
+
+            <div className="stat">
+                <div className="stat-figure text-primary">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-8 h-8 stroke-current">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                    </svg>
+                </div>
+                <div className="stat-title">Total Following</div>
+                <div className="stat-value text-primary">{followingCount}</div>
+            </div>
+
+            <div className="stat">
+                <div className="stat-figure text-secondary">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-8 h-8 stroke-current">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                    </svg>
+                </div>
+                <div className="stat-title">Page Reviews</div>
+                <div className="stat-value text-secondary">{ReviewCount}</div>
             </div>
         </div>
     )
