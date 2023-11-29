742c326922510eacbbda7c8944ea1d169f5a67ad
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-29 16:53:39 +0900

    로그인 버튼 이벤트 추가

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
-import {Button, LoginUseButton, OAuthButton, Title} from '../../components'
-import {EventTargetContext} from 'react-naver-maps'
+import {LoginUseButton, OAuthButton, Title} from '../../components'
 import LoginInput from '../../components/Login/LoginInput'
+import google from '../../assets/google.svg'
 
 export const Login = () => {
     const [userEmail, setUserEmail] = useState<string>('')
@@ -17,8 +17,15 @@ export const Login = () => {
         setUserPassword(value)
     }
 
-    //로그인 버튼
+    //로그인 버튼 클릭 이벤트
     function onLoginClick() {
+        if (userEmail === '') {
+            alert('이메일을 입력하세요')
+            return
+        } else if (userPassword === '') {
+            alert('비밀번호를 입력하세요')
+            return
+        }
         console.log(userEmail, userPassword)
     }
 
@@ -31,7 +38,6 @@ export const Login = () => {
                         <div className="flex flex-wrap items-center justify-center h-fit g-6 lg:justify-center">
                             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
                                 <div>
-                                    {/* 추후 병합 후 assets파일로 옮길 예정 */}
                                     <img
                                         // src="https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg"
                                         src="https://images.unsplash.com/photo-1655722723663-75b47de17a31?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -77,39 +83,8 @@ export const Login = () => {
                                     </div>
 
                                     <OAuthButton color="bg-slate-100">
-                                        {/* 구글 로고 - 추후 병합 후에 assets파일로 옮길 예정 */}
-                                        <svg
-                                            className="mr-2"
-                                            width="15"
-                                            height="15"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M23.04 12.2614C23.04 11.4459 22.9668 10.6618 22.8309 9.90912H12V14.3575H18.1891C17.9225 15.795 17.1123 17.013 15.8943 17.8284V20.7139H19.6109C21.7855 18.7118 23.04 15.7637 23.04 12.2614Z"
-                                                fill="#4285F4"
-                                            />
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M12 23.4998C15.105 23.4998 17.7081 22.47 19.6109 20.7137L15.8943 17.8282C14.8645 18.5182 13.5472 18.9259 12 18.9259C9.00474 18.9259 6.46951 16.903 5.56519 14.1848H1.72314V17.1644C3.61542 20.9228 7.50451 23.4998 12 23.4998Z"
-                                                fill="#34A853"
-                                            />
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M5.56523 14.1851C5.33523 13.4951 5.20455 12.758 5.20455 12.0001C5.20455 11.2421 5.33523 10.5051 5.56523 9.81506V6.83551H1.72318C0.944318 8.38801 0.5 10.1444 0.5 12.0001C0.5 13.8557 0.944318 15.6121 1.72318 17.1646L5.56523 14.1851Z"
-                                                fill="#FBBC05"
-                                            />
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M12 5.07386C13.6884 5.07386 15.2043 5.65409 16.3961 6.79364L19.6945 3.49523C17.7029 1.63955 15.0997 0.5 12 0.5C7.50451 0.5 3.61542 3.07705 1.72314 6.83545L5.56519 9.815C6.46951 7.09682 9.00474 5.07386 12 5.07386Z"
-                                                fill="#EA4335"
-                                            />
-                                        </svg>
+                                        {/* 구글 로고 */}
+                                        <img className="mr-2" src={google} />
                                         Continue with Google
                                     </OAuthButton>
                                     <OAuthButton
