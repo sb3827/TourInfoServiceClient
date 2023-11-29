9cf2f8ba7d55962b52073ad5e38d31d8d76fe7a7
honghyibeom <ghdgmlqja1@naver.com>
2023-11-29 18:54:13 +0900

    like -> likeCount

--- src/components/place/Board.tsx
+++ src/components/place/Board.tsx
@@ -5,10 +5,10 @@ type BoardProps = {
     title: string
     imageUrl: string
     rating: string
-    like: string
+    likeCount: string
 }
 
-export const Board: FC<BoardProps> = ({title, imageUrl, rating, like}) => {
+export const Board: FC<BoardProps> = ({title, imageUrl, rating, likeCount}) => {
     // 추후 length 값을 배열로 나타내야함
     const imageArray = Array.from({length: 3}, (_, index) => (
         <figure key={index}>
@@ -17,12 +17,14 @@ export const Board: FC<BoardProps> = ({title, imageUrl, rating, like}) => {
     ))
 
     return (
-        <div className="w-full shadow-xl card bg-base-100">
-            <Slider>{imageArray}</Slider>
-            <div className="flex justify-between card-body">
-                <h2 className="card-title">
-                    {title} {rating} {like}
-                </h2>
+        <div className="flex justify-center">
+            <div className="w-5/6 shadow-xl card bg-base-100">
+                <Slider>{imageArray}</Slider>
+                <div className="flex justify-between card-body">
+                    <h2 className="card-title">
+                        {title} {rating} {likeCount}
+                    </h2>
+                </div>
             </div>
         </div>
     )
