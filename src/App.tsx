032e6daa98151db81cedc47e703f9ad0e5648ac4
sb3827 <mmk275@naver.com>
2023-11-29 12:20:58 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import {Nmap} from './components'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <Nmap></Nmap>
+        </div>
+    )
 }
 
 export default App
