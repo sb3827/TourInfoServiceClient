5b47ef0bc04fa334016067603fc2ce50d743e9cb
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-11-30 00:41:36 +0900

    Update App.tsx
    Nmap제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import './App.css'
-import {Nmap} from './components'
 
 function App() {
     return (
         <div className="App">
-            <Nmap></Nmap>
         </div>
     )
 }
