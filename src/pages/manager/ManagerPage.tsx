a8cdac704f8824ca1fa4ad69b6e1cdda5a22a92e
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-01 19:18:19 +0900

    페이지 구조 변경 - Toggle버튼 삭제

--- src/pages/manager/ManagerPage.tsx
+++ src/pages/manager/ManagerPage.tsx
@@ -11,7 +11,7 @@ import {
     WaitBox
 } from '../../components/index'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBell, faList} from '@fortawesome/free-solid-svg-icons'
+import {faBell, faList, faUser} from '@fortawesome/free-solid-svg-icons'
 
 //관리자 페이지
 
@@ -26,7 +26,7 @@ export const Manager: FC<ManagerProps> = ({}) => {
     function onChangeSearch(value: string) {
         setSearchValue(value)
     }
-    //select 값 업데이트
+    //사용자 검색 select 값 업데이트
     function onChangeSelect(e: React.ChangeEvent<HTMLSelectElement>) {
         setSelectValue(e.target.value)
     }
@@ -38,47 +38,75 @@ export const Manager: FC<ManagerProps> = ({}) => {
 
     return (
         <Box>
-            <ToggleButton size="w-48">
-                <Subtitle
-                    value="회원대기 목록"
-                    className="flex flex-row-reverse items-center text-left">
-                    <FontAwesomeIcon icon={faList} className="m-1" />
-                </Subtitle>
-                <Subtitle
-                    value="신고 목록"
-                    className="flex flex-row-reverse items-center text-left">
-                    <FontAwesomeIcon icon={faBell} className="m-1" />
-                </Subtitle>
-                <WaitBox />
-                <ReportBox />
-            </ToggleButton>
-
             <div className="w-2/3 ml-4">
-                <Subtitle value="사용자 검색" className="flex items-center m-5">
-                    <DropdownSelect>
-                        <select
-                            onChange={onChangeSelect}
-                            value={selectValue}
-                            className="block w-full py-3 pl-3 pr-10 leading-tight border border-gray-300 shadow appearance-none rounded-2xl focus:outline-none focus:shadow-outline">
-                            <option value="all">전체</option>
-                            <option value="nomal">일반 유저</option>
-                            <option value="business">사업자</option>
-                            <option value="freeze">정지된 유저</option>
-                        </select>
-                    </DropdownSelect>
-                    <SearchInput
-                        className="w-1/2"
-                        value={searchValue}
-                        onChange={onChangeSearch}
-                    />
-                    <Button
-                        onClick={onSubmitSearch}
-                        value="검색"
-                        className="text-center text-white bg-purple-600"
-                    />
-                </Subtitle>
+                <div className="flex items-center m-5">
+                    <FontAwesomeIcon icon={faUser} className="m-1" />
+                    <Subtitle value="사용자 검색" className="flex items-center w-full">
+                        <DropdownSelect>
+                            <select
+                                onChange={onChangeSelect}
+                                value={selectValue}
+                                className="block w-full py-3 pl-3 pr-10 leading-tight border border-gray-300 shadow appearance-none rounded-2xl focus:outline-none focus:shadow-outline">
+                                <option value="all">전체</option>
+                                <option value="nomal">일반 유저</option>
+                                <option value="business">사업자</option>
+                                <option value="freeze">정지된 유저</option>
+                            </select>
+                        </DropdownSelect>
+                        <SearchInput
+                            className="w-1/2"
+                            value={searchValue}
+                            onChange={onChangeSearch}
+                        />
+                        <Button
+                            onClick={onSubmitSearch}
+                            value="검색"
+                            className="text-center text-white bg-purple-600"
+                        />
+                    </Subtitle>
+                </div>
             </div>
             <FindBox />
+
+            <div className="w-2/3 ml-4">
+                <div className="flex items-center m-5">
+                    <FontAwesomeIcon icon={faList} className="m-1" />
+                    <Subtitle
+                        value="회원대기 목록"
+                        className="flex items-center w-full"
+                    />
+                </div>
+            </div>
+            <WaitBox />
+
+            <div className="w-2/3 ml-4">
+                <div className="flex items-center m-5">
+                    <FontAwesomeIcon icon={faBell} className="m-1" />
+                    <Subtitle value="신고 목록" className="flex items-center w-full">
+                        <DropdownSelect>
+                            <select
+                                onChange={onChangeSelect}
+                                value={selectValue}
+                                className="block w-full py-3 pl-3 pr-10 leading-tight border border-gray-300 shadow appearance-none rounded-2xl focus:outline-none focus:shadow-outline">
+                                <option value="all">전체</option>
+                                <option value="nomal">처리 중</option>
+                                <option value="business">처리 완료</option>
+                            </select>
+                        </DropdownSelect>
+                        <SearchInput
+                            className="w-1/2"
+                            value={searchValue}
+                            onChange={onChangeSearch}
+                        />
+                        <Button
+                            onClick={onSubmitSearch}
+                            value="검색"
+                            className="text-center text-white bg-purple-600"
+                        />
+                    </Subtitle>
+                </div>
+            </div>
+            <ReportBox />
         </Box>
     )
 }
