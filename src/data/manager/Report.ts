a0cf0016cb776f6f27fd6def9d0495da5b869ae0
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-21 01:05:59 +0900

    api통신으로 얻는 데이터와 신고에 대한 데이터

--- src/data/manager/Report.ts
+++ src/data/manager/Report.ts
@@ -1,8 +1,32 @@
+//신고 정보 요청 결과
+export type ReportResponseData = {
+    result: boolean
+    data: Array<{
+        sno: number
+        complainant_mno: number
+        complainant: string
+        defendant_mno: number
+        defendant: string
+        bno: number | null
+        rno: number | null
+        content: string
+        isDone: boolean
+        message: string
+        regDate: string
+    }>
+}
+
 //신고 정보
 export type ReportData = {
-    reportDate: Date //날짜
-    listNum: number //게시글 번호
-    userId: string //아이디
-    reportedUser: string //신고 유저
-    reportDetail: string //신고사유
+    sno: number
+    complainant_mno: number
+    complainant: string
+    defendant_mno: number
+    defendant: string
+    bno: number | null
+    rno: number | null
+    content: string
+    isDone: boolean
+    message: string
+    regDate: string
 }
