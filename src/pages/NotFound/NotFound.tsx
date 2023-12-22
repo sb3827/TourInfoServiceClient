9b3d489fc43a842c3618590ec5a70c79846cc4a3
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-22 17:21:54 +0900

    not found 페이지 추가 -> 생성만 했음

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+
+type NotFoundProps = {}
+
+export const NotFound: FC<NotFoundProps> = ({}) => {
+    return (
+        <>
+            <div>NotFound Component</div>
+        </>
+    )
+}
