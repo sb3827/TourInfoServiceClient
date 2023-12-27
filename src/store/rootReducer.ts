8e3fb4c575b3d861ba709922d98d8e1cf7d21c52
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-27 17:38:49 +0900

    로그인 리듀서 등록

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 import ReportSlice from './slices/ReportSlice'
+import LoginSlice from './slices/LoginSlice'
 
 //리듀서 등록
 export const rootReducer = combineReducers({
-    report: ReportSlice.reducer
+    report: ReportSlice.reducer,
+    login: LoginSlice.reducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
