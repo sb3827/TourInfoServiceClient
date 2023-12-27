c38e729463ffb6068494c8d09d2c27e345a9e3fb
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-27 15:19:00 +0900

    토큰이 만료된경우 refresh 토큰으로 재발급

---
+++ src/api/refresh.ts
@@ -0,0 +1,36 @@
+import axios, {AxiosRequestConfig, InternalAxiosRequestConfig} from 'axios'
+import Cookie from 'js-cookie'
+import {getCookie} from '../util/cookie'
+import {getWithTokenExpire, setWithTokenExpire} from '../util/localStorage'
+
+const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
+    const refreshToken = getCookie('refreshToken')
+
+    let token = getWithTokenExpire('token')
+
+    // 토큰이 만료되었고, refreshToken 이 저장되어 있을 때
+    if (token == null && refreshToken) {
+        const body = {
+            refreshToken
+        }
+        // 토큰 갱신 서버통신
+        const response = await axios.post(
+            `${process.env.REACT_APP_YAYAUM_ADDRESS}/auth/newToken`,
+            body
+        )
+
+        token = response.data.token
+        setWithTokenExpire('token', token)
+        if (!config.headers) {
+            config.headers = {}
+        }
+        config.headers['Authorization'] = `Bearer ${token}` // 토큰을 헤더에 설정
+    }
+    return config
+}
+
+const refreshErrorHandle = (err: any) => {
+    Cookie.remove('refreshToken')
+}
+
+export {refresh, refreshErrorHandle}
