a88ce428c3cb3b2df77f7524afc8652948888287
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-27 15:19:12 +0900

    오타 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
-import {Login, Manager, NotFound, Sidebar} from './pages'
+import {Login, Main, Manager, NotFound, Sidebar} from './pages'
 
 import {Footer} from './components'
 import {Route, Routes} from 'react-router-dom'
-import {Signup} from './pages/Signup'
-import {Main} from './pages/Main'
-import {Find} from './pages/Find'
 import {BoardRoute, MyPageRoute} from './routers'
+import {Signup} from './pages/Signup'
+import {Find} from './pages/Find'
 
 //FIXME - 윤서 -Main,Find,Signup pages index파일에 넣어주세요, footer은 공통으로 쓰는 컴포넌트라서 제거하셔도 됩니다
 
@@ -23,7 +22,7 @@ function App() {
                         <Route path="mypage/*" element={<MyPageRoute />} />
                     </Route>
                     <Route path="/login" element={<Login />} />
-                    <Route path="/sigu-up" element={<Signup />} />
+                    <Route path="/sign-up" element={<Signup />} />
                     <Route path="/find-userinfo" element={<Find />} />
                 </Route>
                 <Route path="*" element={<NotFound />} />
