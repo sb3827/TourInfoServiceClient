115edf274f9eb6eae9039d47dadbc40448cf69d8
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-27 15:17:51 +0900

    reportAxios->refreshAxios로 수정

--- src/api/Report/Report.ts
+++ src/api/Report/Report.ts
@@ -1,18 +1,18 @@
 import {ReportCheckData, ReportResponseData} from '../../data/manager'
-import {reportAxios} from './axios'
+import {refreshAxios} from '../Refresh/axios'
 
 //신고 들고오기
 export const getAllReport = async (
     filter: string,
     search: string
 ): Promise<ReportResponseData> => {
-    const response = await reportAxios.get(`/report?filter=${filter}&search=${search}`)
+    const response = await refreshAxios.get(`/report?filter=${filter}&search=${search}`)
     return response.data
 }
 
 //신고 상태 업데이트 -> 신고 처리 완료
 export const checkReport = async (sno: number): Promise<ReportCheckData> => {
-    const response = await reportAxios.put(`/report/update/${sno}`)
+    const response = await refreshAxios.put(`/report/update/${sno}`)
     return response.data
 }
 
@@ -22,7 +22,7 @@ export const disciplinary = async (
     mno: number,
     reason: string
 ): Promise<ReportCheckData> => {
-    const response = await reportAxios.post('/report/disciplinary', {
+    const response = await refreshAxios.post('/report/disciplinary', {
         sno,
         mno,
         reason
