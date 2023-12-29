f5f9d648388124f6412a96dd0fa51f171fdf411c
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-29 11:58:40 +0900

    메인 페이지 디자인 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,18 +1,33 @@
 import {FC, useState} from 'react'
-import {SearchInput, Slider, Title, Subtitle, Footer} from '../../components'
+import {
+    SearchInput,
+    Title,
+    Subtitle,
+    Button,
+    MainItem,
+    MostLikedCourseItem,
+    MostLikedMainItem,
+    CoursePostMap,
+    MainSlider
+} from '../../components'
 import dummyPic from '../../dummy data/dummypic.jpg'
 import dummyPic2 from '../../dummy data/dummypic2.jpg'
-import {MainItem, MostLikedMainItem, MostLikedCourseItem} from './index'
+import mainLogo from '../../assets/mainLogo.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMapLocationDot, faRoute, faUsers} from '@fortawesome/free-solid-svg-icons'
+import MainFilter from '../../components/Main/MainFilter'
+import {SwiperSlide} from 'swiper/react'
 
 type MainProps = {}
 
 export const Main: FC<MainProps> = () => {
-    const [searchValue, setSearchValue] = useState('')
+    const [searchValue, setSearchValue] = useState<string>('')
+    const [filterValue, setFilterValue] = useState<string>('place')
 
     // 검색
     function onSearch(value: string) {
         setSearchValue(value)
-        console.log('searchInputValue : ' + value)
+        console.log('searchInputValue : ' + filterValue + value)
     }
 
     // 상세 페이지 이동
@@ -20,21 +35,61 @@ export const Main: FC<MainProps> = () => {
         alert('상세페이지 이동')
     }
 
+    //filterValue 변경
+    function onSetFilterValue(filter: string) {
+        setFilterValue(filter)
+    }
+
     return (
-        <div>
-            <div className="mt-10">
-                <Title>야! 먹고놀자</Title>
-                <div>
-                    <SearchInput
-                        value={searchValue}
-                        className="m-10"
-                        onChange={onSearch}
-                    />
+        <div className="flex justify-center w-full">
+            <div className="w-4/5 mt-10 ">
+                <Title className="flex justify-center mt-10 cursor-pointer mb-14">
+                    <img src={mainLogo} className="w-32 sm:w-32 md:w-52 lg:w-64" />
+                </Title>
+                <div className="flex flex-col items-center justify-center">
+                    <div className="flex justify-around w-1/3 py-2 mb-3 bg-slate-100 rounded-xl">
+                        <MainFilter
+                            text="장소"
+                            filterChange={onSetFilterValue}
+                            color={
+                                filterValue === 'place' ? 'bg-lightBrown text-white' : ''
+                            }>
+                            <FontAwesomeIcon
+                                icon={faMapLocationDot}
+                                color="bg-lightBrown"
+                            />
+                        </MainFilter>
+                        <MainFilter
+                            text="코스"
+                            filterChange={onSetFilterValue}
+                            color={
+                                filterValue === 'course' ? 'bg-lightBrown text-white' : ''
+                            }>
+                            <FontAwesomeIcon icon={faRoute} color="bg-lightBrown" />
+                        </MainFilter>
+                        <MainFilter
+                            text="유저"
+                            filterChange={onSetFilterValue}
+                            color={
+                                filterValue === 'user' ? 'bg-lightBrown text-white' : ''
+                            }>
+                            <FontAwesomeIcon icon={faUsers} color="bg-lightBrown" />
+                        </MainFilter>
+                    </div>
+                    <div className="flex justify-center w-full mb-12">
+                        <SearchInput
+                            value={searchValue}
+                            className="w-2/3"
+                            onChange={onSearch}
+                        />
+                        <Button className="text-white bg-darkGreen" value={'검색'} />
+                    </div>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Subtitle className="block mx-10 mb-5 font-bold text-left text-cyan-600">
-                        "많은 이들이 극찬한 명소, 그 인기의 이유를 직접 확인하세요"
-                    </Subtitle>
+                    <Subtitle
+                        className="mb-5 font-bold text-cyan-600"
+                        value='" 많은 이들이 극찬한 명소, 그 인기의 이유를 직접 확인하세요 "'
+                    />
                     <div className="container mx-10" onClick={detailView}>
                         <div className="w-full h-full mb-20">
                             <MostLikedMainItem
@@ -46,134 +101,222 @@ export const Main: FC<MainProps> = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col mx-10 mb-40 ">
-                    <Subtitle className="flex items-start mt-10 mb-4 font-bold text-cyan-600">
-                        최근 올라온 장소
-                    </Subtitle>
-                    <Slider>
-                        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div className="px-5 py-10 mb-20 rounded-3xl bg-emerald-50">
+                    <Subtitle
+                        className="flex items-start mb-4 ml-16 font-bold text-cyan-600"
+                        value="최근 올라온 장소"
+                    />
+                    <MainSlider preView={3}>
+                        <SwiperSlide>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
-                        </div>
-                        <div>content2</div>
-                    </Slider>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <MainItem
+                                title="장소명"
+                                description="장소 설명"
+                                image={dummyPic2}
+                                onClick={detailView}
+                            />
+                        </SwiperSlide>
+                    </MainSlider>
                 </div>
 
-                <div className="flex flex-col mx-10 mb-40">
-                    <Subtitle className="flex items-start mt-10 mb-4 font-bold text-cyan-600">
-                        가장 많은 추천을 받은 코스
-                    </Subtitle>
-                    <div className="grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                        <MostLikedCourseItem
-                            title="첫째 날"
-                            description="코스 설명"
-                            image={dummyPic2}
-                            onClick={detailView}
-                        />
-                        <MostLikedCourseItem
-                            title="둘째 날"
-                            description="코스 설명"
-                            image={dummyPic2}
-                            onClick={detailView}
-                        />
-                        <MostLikedCourseItem
-                            title="셋째 날"
-                            description="코스 설명"
-                            image={dummyPic2}
-                            onClick={detailView}
-                        />
-                    </div>
+                <div className="px-5 py-10 mb-20 rounded-3xl bg-sky-50">
+                    <Subtitle
+                        className="flex items-start mb-4 ml-16 font-bold text-cyan-600"
+                        value="가장 많은 추천을 받은 코스"
+                    />
+
+                    <MainSlider preView={1}>
+                        <SwiperSlide>
+                            <div className="flex">
+                                <MostLikedCourseItem
+                                    title="제목"
+                                    description="코스 설명"
+                                    image={dummyPic2}
+                                    onClick={detailView}
+                                />
+                                <CoursePostMap
+                                    className="flex-1 rounded-lg shadow-md"
+                                    places={[
+                                        {
+                                            name: 'abcd',
+                                            lat: 37.74913611,
+                                            lng: 128.8784972,
+                                            local: 'test',
+                                            eng: 'test',
+                                            road: 'test'
+                                        },
+                                        {
+                                            name: 'test',
+                                            lat: 38.37796111,
+                                            lng: 128.4701639,
+                                            local: 'aaaa',
+                                            eng: 'aaaa',
+                                            road: 'aaaa'
+                                        }
+                                    ]}
+                                />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex">
+                                <MostLikedCourseItem
+                                    title="제목"
+                                    description="코스 설명"
+                                    image={dummyPic2}
+                                    onClick={detailView}
+                                />
+                                <CoursePostMap
+                                    className="flex-1 rounded-lg shadow-md"
+                                    places={[
+                                        {
+                                            name: 'abcd',
+                                            lat: 37.74913611,
+                                            lng: 128.8784972,
+                                            local: 'test',
+                                            eng: 'test',
+                                            road: 'test'
+                                        },
+                                        {
+                                            name: 'test',
+                                            lat: 38.37796111,
+                                            lng: 128.4701639,
+                                            local: 'aaaa',
+                                            eng: 'aaaa',
+                                            road: 'aaaa'
+                                        }
+                                    ]}
+                                />
+                            </div>
+                        </SwiperSlide>
+                    </MainSlider>
                 </div>
 
-                <div className="flex flex-col mx-10 mb-40 ">
-                    <Subtitle className="flex items-start mt-10 mb-4 font-bold text-cyan-600">
-                        팔로워들의 게시물
-                    </Subtitle>
-                    <Slider>
-                        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div className="px-5 py-10 mb-20 rounded-3xl bg-blue-50">
+                    <Subtitle
+                        className="flex items-start mb-4 ml-16 font-bold text-cyan-600"
+                        value="팔로워들의 게시물"
+                    />
+                    <MainSlider preView={3}>
+                        <SwiperSlide key={1} virtualIndex={1}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide key={2} virtualIndex={2}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide key={3} virtualIndex={3}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide key={4} virtualIndex={4}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
-                        </div>
-                        <div>content2</div>
-                    </Slider>
+                        </SwiperSlide>
+                        <SwiperSlide key={5} virtualIndex={5}>
+                            <MainItem
+                                title="장소명"
+                                description="장소 설명"
+                                image={dummyPic2}
+                                onClick={detailView}
+                            />
+                        </SwiperSlide>
+                    </MainSlider>
                 </div>
-                <div className="bg-orange-50 h-150">
-                    <div className="flex flex-col mx-10 mb-40 ">
-                        <Subtitle className="flex items-start mt-10 mb-4 font-bold ">
-                            광고
-                        </Subtitle>
-                        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div className="px-5 py-10 mb-20 rounded-3xl bg-orange-50">
+                    <Subtitle
+                        className="flex items-start mb-4 ml-16 font-bold "
+                        value="광고"
+                    />
+                    <MainSlider preView={3}>
+                        <SwiperSlide key={1} virtualIndex={1}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide key={2} virtualIndex={2}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide key={3} virtualIndex={3}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
+                        </SwiperSlide>
+                        <SwiperSlide key={4} virtualIndex={4}>
                             <MainItem
                                 title="장소명"
                                 description="장소 설명"
                                 image={dummyPic2}
                                 onClick={detailView}
                             />
-                        </div>
-                    </div>
-                    <Footer></Footer>
+                        </SwiperSlide>
+                        <SwiperSlide key={5} virtualIndex={5}>
+                            <MainItem
+                                title="장소명"
+                                description="장소 설명"
+                                image={dummyPic2}
+                                onClick={detailView}
+                            />
+                        </SwiperSlide>
+                    </MainSlider>
                 </div>
             </div>
         </div>
