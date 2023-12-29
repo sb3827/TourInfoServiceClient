0096d0cbad504a91a30f1a7d8b44129b8a3d0e27
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-29 11:54:27 +0900

    refresh 로그아웃시 오류고 수정

--- src/api/refresh.ts
+++ src/api/refresh.ts
@@ -2,14 +2,13 @@ import axios, {AxiosRequestConfig, InternalAxiosRequestConfig} from 'axios'
 import Cookie from 'js-cookie'
 import {getCookie} from '../util/cookie'
 import {getWithTokenExpire, setWithTokenExpire} from '../util/localStorage'
-
 const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
     const refreshToken = getCookie('refreshToken')
 
     let token = getWithTokenExpire('token')
 
     // 토큰이 만료되었고, refreshToken 이 저장되어 있을 때
-    if (token == null && refreshToken) {
+    if (token === null && refreshToken) {
         const body = {
             refreshToken
         }
@@ -21,11 +20,16 @@ const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> 
 
         token = response.data.token
         setWithTokenExpire('token', token)
-        if (!config.headers) {
-            config.headers = {}
-        }
-        config.headers['Authorization'] = `Bearer ${token}` // 토큰을 헤더에 설정
     }
+    if (!config.headers) {
+        config.headers = {}
+    }
+    if (refreshToken === null) {
+        alert('토큰 만료')
+        throw new Error('Refresh token expired')
+    }
+    config.headers['Authorization'] = `Bearer ${token}` // 토큰을 헤더에 설정
+
     return config
 }
 
