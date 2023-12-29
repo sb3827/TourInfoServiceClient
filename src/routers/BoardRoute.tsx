a9459837c33bf6f1cff28a4fb1406a86865c6b2b
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-29 11:59:44 +0900

    board 라우팅

--- src/routers/BoardRoute.tsx
+++ src/routers/BoardRoute.tsx
@@ -1,14 +1,39 @@
 import {FC} from 'react'
 import {Route, Routes} from 'react-router-dom'
-import {NotFound} from '../pages'
+import {CourseRegister, DetailedCourse, NotFound, PostPlace, PostRegister} from '../pages'
+import {PlaceSearch} from '../pages/placeSearch'
+import {PlaceDetails} from '../pages/placeDetails'
+import {CourseSearch} from '../pages/CourseSearch'
+import {CoursePostMap} from '../components'
 
 type BoardRouteProps = {}
 
 export const BoardRoute: FC<BoardRouteProps> = ({}) => {
     return (
         <Routes>
-            <Route path="/place"></Route>
-            <Route path="/course"></Route>
+            <Route path="/place">
+                <Route index element={<PlaceSearch />} />
+                <Route path=":pno" element={<PlaceDetails />} />
+                <Route
+                    path="posting/register"
+                    element={<PostRegister isModify={false} />}
+                />
+                <Route path="posting/modify" element={<PostRegister isModify={true} />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+            <Route path="/course">
+                <Route index element={<CourseSearch />} />
+                <Route path=":bno" element={<DetailedCourse title="test" />} />
+                <Route
+                    path="posting/register"
+                    element={<CourseRegister isModify={false} />}
+                />
+                <Route
+                    path="posting/modify"
+                    element={<CourseRegister isModify={true} />}
+                />
+                <Route path="*" element={<NotFound />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
