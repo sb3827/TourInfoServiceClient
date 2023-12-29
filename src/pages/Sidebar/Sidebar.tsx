f9c22702681bdfe69d265a29eee0946717d8ebc8
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-29 11:59:31 +0900

    사이드바에서 유저는 refreshToken 유무로 판단

--- src/pages/Sidebar/Sidebar.tsx
+++ src/pages/Sidebar/Sidebar.tsx
@@ -8,15 +8,15 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import {Outlet, useNavigate} from 'react-router-dom'
 import {SidebarItem, SidebarTitle} from '../../components'
-import Cookies from 'js-cookie'
 import {useSelector} from 'react-redux'
 import {RootState} from '../../store/rootReducer'
 import {SidebarRoute} from '../../routers'
+import {getCookie} from '../../util/cookie'
 
 type SidebarProps = {}
 
 export const Sidebar: FC<PropsWithChildren<SidebarProps>> = ({children}) => {
-    const user = useSelector((state: RootState) => state.login.user)
+    const user = getCookie('refreshToken')
 
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
@@ -30,17 +30,21 @@ export const Sidebar: FC<PropsWithChildren<SidebarProps>> = ({children}) => {
     }
 
     //장소 게시글 페이지로 이동
-    function onPlace() {}
+    function onPlace() {
+        navigate('/board/place')
+    }
 
     //코스 게시글 페이지로 이동
-    function onCourse() {}
+    function onCourse() {
+        navigate('/board/course')
+    }
 
     return (
         <div className="z-50">
             <div className="h-full ml-auto">
                 <div>{children}</div>
                 <div
-                    className={` rounded-tr-3xl fixed top-0 w-14 h-full bg-gradient-to-b from-blue-300  transition-all duration-200 pt-4 flex-col items-center  ${
+                    className={`z-50 rounded-tr-3xl fixed top-0 w-14 h-full bg-gradient-to-b from-darkGreen transition-all duration-200 pt-4 flex-col items-center  ${
                         isOpen ? 'w-64' : ''
                     }`}>
                     <div
@@ -57,21 +61,21 @@ export const Sidebar: FC<PropsWithChildren<SidebarProps>> = ({children}) => {
                     {/* 서버 연결후 로그인 하지 않은 회원과 관리자 로그인했을때 SidebarItem 바꿔줄 필요 있음 */}
                     {/* 메인 페이지로 이동 */}
                     <SidebarItem sideTitle="Yum" isOpen={isOpen} onClick={onMain}>
-                        <p className="font-bold text-blue-500">Ya!</p>
+                        <p className="font-bold">Ya!</p>
                     </SidebarItem>
                     {/* 장소 게시판으로 이동 */}
                     <SidebarItem
                         sideTitle="장소 게시판"
                         isOpen={isOpen}
                         onClick={onPlace}>
-                        <FontAwesomeIcon icon={faMapLocationDot} color="#4169E1" />
+                        <FontAwesomeIcon icon={faMapLocationDot} />
                     </SidebarItem>
                     {/* 코스 게시판으로 이동 */}
                     <SidebarItem
                         sideTitle="코스 게시판"
                         isOpen={isOpen}
                         onClick={onCourse}>
-                        <FontAwesomeIcon icon={faRoute} color="#4169E1" />
+                        <FontAwesomeIcon icon={faRoute} />
                     </SidebarItem>
 
                     <SidebarRoute isOpen={isOpen} />
