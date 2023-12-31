96998770c7f9a48e0ef2318d2cba7cc416c2a440
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-31 18:47:48 +0900

    로그인 페이지 뿐만 아니라 회원가입 등의 페이지도 설정이 필요해서 AuthRoute로 변경하고 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import {Login, Main, Manager, NotFound, Sidebar} from './pages'
 import {Footer} from './components'
 import {Route, Routes} from 'react-router-dom'
-import {BoardRoute, LoginRoute, MyPageRoute, Oauth2Route} from './routers'
+import {AuthRoute, BoardRoute, MyPageRoute, Oauth2Route} from './routers'
 import {Signup} from './pages/Signup'
 import {Find} from './pages/Find'
 
@@ -20,11 +20,13 @@ function App() {
                         <Route path="board/*" element={<BoardRoute />} />
                         <Route path="mypage/*" element={<MyPageRoute />} />
                     </Route>
-                    <Route path="/login" element={<LoginRoute />} />
-                    <Route path="/sign-up" element={<Signup />} />
-                    <Route path="/find-userinfo" element={<Find />} />
+                    <Route element={<AuthRoute />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/sign-up" element={<Signup />} />
+                        <Route path="/find-userinfo" element={<Find />} />
+                        <Route path="/oauth2" element={<Oauth2Route />} />
+                    </Route>
                 </Route>
-                <Route path="/oauth2" element={<Oauth2Route />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
--- src/routers/LoginRoute.tsx
+++
@@ -1,8 +0,0 @@
-import {Navigate} from 'react-router-dom'
-import {Login} from '../pages'
-import {getCookie} from '../util/cookie'
-
-export const LoginRoute = () => {
-    const refreshToken = getCookie('refreshToken')
-    return refreshToken ? <Navigate to={'/'} /> : <Login />
-}
