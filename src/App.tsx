131a91ee2b47de652a4fbc026a74a9e825ee79b7
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-31 22:00:08 +0900

    유저 검색 페이지 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import {Route, Routes} from 'react-router-dom'
 import {AuthRoute, BoardRoute, MyPageRoute, Oauth2Route} from './routers'
 import {Signup} from './pages/Signup'
 import {Find} from './pages/Find'
+import {UserSearch} from './pages/UserSearch'
 
 //FIXME - 윤서 -Main,Find,Signup pages index파일에 넣어주세요, footer은 공통으로 쓰는 컴포넌트라서 제거하셔도 됩니다
-
+//        해창 - UserSearch pages에 있는 index파일에도 추가해주세요.
 function App() {
     return (
         <div className="App">
@@ -19,6 +20,7 @@ function App() {
                         {/* 아직 작성x - 추후 수정 */}
                         <Route path="board/*" element={<BoardRoute />} />
                         <Route path="mypage/*" element={<MyPageRoute />} />
+                        <Route path="search-user" element={<UserSearch />} />
                     </Route>
                     <Route element={<AuthRoute />}>
                         <Route path="/login" element={<Login />} />
