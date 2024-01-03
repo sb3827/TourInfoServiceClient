f3a7a2cf84c170330aed01dab19ca6b9ed03c305
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-03 09:35:01 +0900

    메인 필터 바탕색 변경

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -42,7 +42,7 @@ export const Main: FC<MainProps> = () => {
         <div className="flex justify-center w-full">
             <div className="w-4/5 mt-10 ">
                 <div className="flex flex-col items-center justify-center">
-                    <div className="flex justify-around w-1/3 py-2 mb-3 bg-slate-100 rounded-xl">
+                    <div className="flex justify-around w-1/3 py-2 mb-3 bg-green-50 rounded-xl">
                         <MainFilter
                             text="장소"
                             filterChange={onSetFilterValue}
