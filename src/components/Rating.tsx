7a8ac3851b9eca3b7082be087e8885a09d981050
sb3827 <mmk275@naver.com>
2024-01-03 15:56:49 +0900

    Rating Component Ref 수정

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,34 +1,46 @@
-import {FC} from 'react'
+import React, {FC, useState, forwardRef, useImperativeHandle, Ref} from 'react'
 
-// rating component
-export const Rating: FC = () => {
-    return (
-        <div className="rating rating-md">
-            <input
-                type="radio"
-                name="rating-7"
-                className="bg-orange-400 mask mask-star-2"
-            />
-            <input
-                type="radio"
-                name="rating-7"
-                className="bg-orange-400 mask mask-star-2"
-            />
-            <input
-                type="radio"
-                name="rating-7"
-                className="bg-orange-400 mask mask-star-2"
-            />
-            <input
-                type="radio"
-                name="rating-7"
-                className="bg-orange-400 mask mask-star-2"
-            />
-            <input
-                type="radio"
-                name="rating-7"
-                className="bg-orange-400 mask mask-star-2"
-            />
-        </div>
-    )
+// rating props 정의
+type RatingProps = {
+    onChange: (rating: number) => void
+    ref: Ref<RatingRef>
 }
+
+// ref type 정의 current.getSelectedRating()로 값 도출
+export type RatingRef = {
+    getSelectedRating: () => number | null
+}
+
+// fowardRef를 사용하여 Rating 참조
+export const Rating: FC<RatingProps> = forwardRef<RatingRef, RatingProps>(
+    ({onChange}, ref) => {
+        const [selectedRating, setSelectedRating] = useState<number | null>(null)
+
+        // onClick method
+        const handleRatingChange = (rating: number) => {
+            setSelectedRating(rating) // state 저장
+            onChange(rating)
+        }
+
+        // parent component가 조회 할 수 있도록 Ref Method 공개
+        useImperativeHandle(ref, () => ({
+            getSelectedRating: () => selectedRating
+        }))
+
+        return (
+            <div className="rating rating-md">
+                {[1, 2, 3, 4, 5].map(rating => (
+                    <input
+                        key={rating}
+                        type="radio"
+                        name="rating-7"
+                        className={`bg-orange-400 mask mask-star-2 ${
+                            rating === selectedRating ? 'selected' : ''
+                        }`}
+                        onClick={() => handleRatingChange(rating)}
+                    />
+                ))}
+            </div>
+        )
+    }
+)
