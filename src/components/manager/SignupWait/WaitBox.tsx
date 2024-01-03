a6bc51bf5fb54293b3b6e0f4d0fbcd8f033c01c0
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-03 16:52:16 +0900

    더미 삭제 하고 서버 연결

--- src/components/manager/SignupWait/WaitBox.tsx
+++ src/components/manager/SignupWait/WaitBox.tsx
@@ -1,62 +1,66 @@
-import {FC} from 'react'
-import {SubBox, Subtitle, UserInfoItemBox, WaitUser} from '../../index'
-import {WaitUserData} from '../../../data/User/User'
+import {FC, useEffect, useState} from 'react'
+import {
+    LoadingSppinnerSmall,
+    SubBox,
+    Subtitle,
+    UserInfoItemBox,
+    WaitUser
+} from '../../index'
+import {SignupWaitData} from '../../../data/User/User'
+import {useDispatch} from 'react-redux'
+import {getSignupWait} from '../../../api/Manager/Manager'
 
 //회원 대기 목록 컴포넌트 합체 - 추후 WaitUser에 props 추가하여야하고 반복문으로 수정해야함
 
 type WaitBoxProps = {}
 
 export const WaitBox: FC<WaitBoxProps> = ({}) => {
-    //사업자 회원대기 더미 데이터 ////////////////////////
-    const waitDummy: WaitUserData[] = [
-        {
-            userName: '사업자1',
-            userId: '사업자 아이디1',
-            userEmail: 'email@test.com',
-            phoneNum: '01011112222',
-            businessNum: '0000000000'
-        },
-        {
-            userName: '사업자2',
-            userId: '사업자 아이디2',
-            userEmail: 'email2@test.com',
-            phoneNum: '01023456789',
-            businessNum: '5691901664'
-        },
-        {
-            userName: '사업자3',
-            userId: '사업자 아이디3',
-            userEmail: 'liame@test.com',
-            phoneNum: '01011110000',
-            businessNum: '1111111111'
+    const dispatch = useDispatch()
+    const [waitData, setWaitData] = useState<Array<SignupWaitData>>()
+    const [loading, setLoading] = useState<Boolean>(false)
+
+    //회원 대기 조회
+    async function onSignupWaitList() {
+        setLoading(true)
+        try {
+            const data = await getSignupWait()
+            setWaitData(data)
+            setLoading(false)
+        } catch (err) {
+            console.log(err)
+            alert('서버와 연결이 끊겼습니다.')
+            setLoading(false)
         }
-    ]
-    ///////////////////////////////////////////
+    }
+    useEffect(() => {
+        onSignupWaitList()
+    }, [])
 
     return (
-        <SubBox>
+        <SubBox className="relative">
             <UserInfoItemBox widthFull={false}>
                 <div className="flex">
+                    <div>
+                        <Subtitle className="w-40 p-3 min-w-fit">회원번호</Subtitle>
+                    </div>
                     <div>
                         <Subtitle className="w-40 p-3 min-w-fit">이름</Subtitle>
                     </div>
                     <div>
-                        <Subtitle className="w-40 p-3 min-w-fit">아이디</Subtitle>
-                    </div>
-                    <div>
                         <Subtitle className="w-40 p-3 min-w-fit">이메일</Subtitle>
-                    </div>
-                    <div>
-                        <Subtitle className="w-40 p-3 min-w-fit">전화번호</Subtitle>
                     </div>
                     <div>
                         <Subtitle className="w-40 p-3 min-w-fit">사업자 번호</Subtitle>
                     </div>
                 </div>
             </UserInfoItemBox>
-            {waitDummy.map((waitUser, index) => (
-                <WaitUser key={index} waitUser={waitUser} />
-            ))}
+
+            {loading && <LoadingSppinnerSmall />}
+
+            {waitData &&
+                waitData.map((waitUser, index) => (
+                    <WaitUser key={index} waitUser={waitUser} />
+                ))}
         </SubBox>
     )
 }
