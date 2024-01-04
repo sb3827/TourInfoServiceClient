a87283e736d4d57e028c8d89273a5587410bbf04
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-04 13:12:56 +0900

    관리자 페이지에서 유저 검색부분 api 값 받아오기 추가

--- src/components/manager/FindUser/FindUserInfo.tsx
+++ src/components/manager/FindUser/FindUserInfo.tsx
@@ -1,44 +1,43 @@
 import {FC, useState} from 'react'
 import {Button, ReportModal, UserInfo, UserInfoItemBox} from '../../index'
-import {UserData, suspendDate} from '../../../data/User/User'
+import {ManagerSearchUserData} from '../../../data/User/User'
+import {getUserDisciplinary} from '../../../api'
+import {DisciplinaryUserData} from '../../../data/manager'
 
 type FindUserInfoProps = {
-    users: UserData
+    users: ManagerSearchUserData
 }
 
 export const FindUserInfo: FC<FindUserInfoProps> = ({users}) => {
     const [isModalOpen, setModalOpen] = useState<boolean>(false)
+    const [disciplinaryData, setDisciplinaryData] = useState<DisciplinaryUserData | null>(
+        null
+    )
 
     const openModal = () => setModalOpen(true)
     const closeModal = () => setModalOpen(false)
 
-    // 정지 이력 더미 데이터
-    const dummyData: suspendDate[] = [
-        {suspendStartDate: new Date(), suspendEndDate: new Date(), suspendReason: '그냥'},
-        {
-            suspendStartDate: new Date(-3),
-            suspendEndDate: new Date(-1),
-            suspendReason: '그냥'
-        },
-        {
-            suspendStartDate: new Date(),
-            suspendEndDate: new Date(+1),
-            suspendReason: '그냥'
+    async function onDisciplinaryAll() {
+        try {
+            const data = await getUserDisciplinary(users.mno)
+            setDisciplinaryData(data)
+        } catch (err) {
+            console.log(err)
+            alert('서버와 연결이 끊겼습니다.')
         }
-    ]
-    //////////////////////////////////////////////////////////
+    }
 
     return (
         <div>
             <UserInfoItemBox widthFull={false} isButton={true}>
                 <div className="flex">
-                    <UserInfo text={users.userName} />
-                    <UserInfo text={users.userId} />
-                    <UserInfo text={users.userEmail} />
-                    <UserInfo text={users.phoneNum} />
-                    <UserInfo text={users.regDate.toDateString()} />
+                    <UserInfo text={users.mno.toString()} />
+                    <UserInfo text={users.name} />
+                    <UserInfo text={users.email} />
+                    <UserInfo text={users.phone ? users.phone : '0'} />
+                    <UserInfo text={users.regDate} />
                     <UserInfo
-                        text={users.businessCheck === true ? '사업자' : '일반 회원'}
+                        text={users.role === 'BUSINESSPERSON' ? '사업자' : '일반 회원'}
                     />
                 </div>
                 <div className="flex justify-end p-3 min-w-fit">
@@ -58,27 +57,25 @@ export const FindUserInfo: FC<FindUserInfoProps> = ({users}) => {
                     <h1 className="text-xl font-bold">유저정보</h1>
                 </div>
 
-                <p className="mt-4">이름 : {users.userName}</p>
-                <p className="mt-4">아이디 : {users.userId}</p>
-                <p className="mt-4">이메일 : {users.userEmail}</p>
-                <p className="mt-4">전화번호 : {users.phoneNum}</p>
-                <p className="mt-4">가입일 : {users.regDate.toDateString()}</p>
+                <p className="mt-4">회원 번호 : {users.mno}</p>
+                <p className="mt-4">이름 : {users.name}</p>
+                <p className="mt-4">이메일 : {users.email}</p>
+                <p className="mt-4">전화번호 : {users.phone ? users.phone : '0'}</p>
+                <p className="mt-4">가입일 : {users.regDate}</p>
                 <p className="mt-4">
-                    사업자여부 : {users.businessCheck === true ? '사업자' : '일반 회원'}
+                    사업자여부 :{' '}
+                    {users.role === 'BUSINESSPERSON' ? '사업자' : '일반 회원'}
                 </p>
 
                 <p className="my-2 font-bold break-all">- 제재 이력 -</p>
                 <div className="flex-col">
-                    {dummyData.map((data, index) => (
+                    {disciplinaryData?.data.map((data, index) => (
                         <p key={index} className="flex justify-between mb-1">
                             <span>
-                                {index + 1}. 정지 시작일 :{' '}
-                                {data.suspendStartDate.toDateString()}
+                                {index + 1}. 정지 시작일 : {data.strDate}
                             </span>{' '}
-                            <span>
-                                정지 종료일 : {data.suspendEndDate.toDateString()}
-                            </span>
-                            <span> 정지 사유 : {data.suspendReason}</span>
+                            <span>정지 종료일 : {data.expDate}</span>
+                            <span> 정지 사유 : {data.reason}</span>
                         </p>
                     ))}
                 </div>
