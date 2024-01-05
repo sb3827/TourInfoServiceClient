4b313bffa53c841adb1e21628b36e8730730be7e
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-05 16:07:45 +0900

    Reply부분 수정

--- src/pages/Reply/Reply.tsx
+++ src/pages/Reply/Reply.tsx
@@ -1,26 +1,54 @@
 import {FC, PropsWithChildren} from 'react'
 import {Input, Button, Comment} from '../../components'
-import {array} from "../../dummy data/sb's dummy"
 
 type ReplyProps = {}
+
+//더미 데이터
+export const replyData = [
+    {
+        rno: 1,
+        name: 'test',
+        src: null,
+        text: 'test',
+        regDate: '2023-12-30',
+        parent_rno: null
+    },
+    {
+        rno: 2,
+        name: 'test',
+        src: null,
+        text: 'test',
+        regDate: '2023-12-30',
+        parent_rno: null
+    },
+    {
+        rno: 3,
+        name: 'test',
+        src: null,
+        text: 'test',
+        regDate: '2023-12-30',
+        parent_rno: null
+    }
+]
+///
 
 // 댓글 part 작성+list
 export const Reply: FC<PropsWithChildren<ReplyProps>> = () => {
     return (
         <div>
-            <div className="flex flex-row justify-center">
+            <div className="flex flex-row items-center justify-center my-5">
                 <Input
                     placeholder="댓글을 작성해 주세요"
                     className="mx-2 border-black"
                     size={80}></Input>
                 <Button value="작성" className="" />
             </div>
-            <div className="flex justify-center">
-                <Comment
-                    img={array[0]}
-                    user={array[1]}
-                    comment={array[2]}
-                    date={array[3]}></Comment>
+            <div className="flex flex-col items-center justify-center w-full ">
+                <div className="w-3/5 rounded-2xl">
+                    {replyData.map(reply => (
+                        <Comment reply={reply} key={reply.rno}></Comment>
+                    ))}
+                </div>
             </div>
         </div>
     )
