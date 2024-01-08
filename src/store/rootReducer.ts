e86b3671d67a5a658fab4601652f0863ffb3e9c4
yununyu <myamoly10@gmail.com>
2024-01-08 12:00:29 +0900

    회원가입 페이지 리듀서 츄가

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux'
 import ReportSlice from './slices/ReportSlice'
 import LoginSlice from './slices/LoginSlice'
 import SearchSlice from './slices/SearchSlice'
+import SignupSlice from './slices/SignupSlice'
 
 //리듀서 등록
 export const rootReducer = combineReducers({
     report: ReportSlice.reducer,
     login: LoginSlice.reducer,
-    search: SearchSlice.reducer
+    search: SearchSlice.reducer,
+    signup: SignupSlice.reducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
