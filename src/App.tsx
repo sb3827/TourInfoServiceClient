4ef09aa652a1f7e097782cfe7e30dd271ee5e9ad
이해창 <gockd9968@naver.com>
2024-01-08 10:04:03 +0900

    usersearchpage index 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
-import {Login, Main, Manager, NotFound, Sidebar} from './pages'
+import {Login, Main, Manager, NotFound, Sidebar, UserSearch} from './pages'
 import {Footer, Logo} from './components'
 import {Route, Routes} from 'react-router-dom'
 import {AuthRoute, BoardRoute, MyPageRoute, Oauth2Route} from './routers'
 import {Signup} from './pages/Signup'
 import {Find} from './pages/Find'
-import {UserSearch} from './pages/UserSearch'
 
 //FIXME - 윤서 -Main,Find,Signup pages index파일에 넣어주세요, footer은 공통으로 쓰는 컴포넌트라서 제거하셔도 됩니다
-//        해창 - UserSearch pages에 있는 index파일에도 추가해주세요.
 function App() {
     return (
         <div className="App">
