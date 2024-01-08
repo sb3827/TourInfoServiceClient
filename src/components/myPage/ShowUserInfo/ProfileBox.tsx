2f78fe2d2780697059195116d4322263c23989ad
이해창 <gockd9968@naver.com>
2024-01-08 10:04:20 +0900

    마이페이지 프로필 작성

--- src/components/myPage/ShowUserInfo/ProfileBox.tsx
+++ src/components/myPage/ShowUserInfo/ProfileBox.tsx
@@ -1,21 +1,57 @@
-import {Box, UserAvatar, UserInfo, ShowFollowModal, ShowTotalLikes} from './../../index'
+import {Box, UserAvatar, ShowFollowModal, ShowTotalLikes} from './../../index'
 import {Button} from './../../Button'
+import {FC, useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {userProfile} from './../../../data/User/User'
+import {ShowUserProfile} from './../../../api/MyPage/ShowUserInfo'
 
-export const ProfileBox = () => {
+type ProfileProps = {}
+
+export const ProfileBox: FC<ProfileProps> = () => {
+    const [userProfile, setUserProfile] = useState<userProfile | null>(null)
+
+    const navigate = useNavigate()
+
+    function onModify() {
+        navigate('/modify')
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userProfileData = await ShowUserProfile('이해창')
+                setUserProfile(userProfileData)
+                console.log(userProfileData)
+            } catch (error) {
+                console.error('에러 발생', error)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
         <div>
             <div>
                 <Box className=" rounded-3xl">
                     <h1 className="mb-4 text-3xl text-black">My Profile</h1>
-                    <UserAvatar />
+                    <UserAvatar src={userProfile ? userProfile.image : ''} />
                     <br />
-                    <h1 className="text-3xl ">문영현</h1>
+                    <h1 className="text-3xl ">{userProfile ? userProfile.name : ''}</h1>
                     <br />
-                    <ShowFollowModal />
+                    <ShowFollowModal
+                        following={userProfile ? userProfile.followings.toString() : ''}
+                        follower={userProfile ? userProfile.followers.toString() : ''}
+                    />
                     <br />
-                    <ShowTotalLikes />
+                    <ShowTotalLikes
+                        cart={userProfile ? userProfile.cart.toString() : ''}
+                    />
                     <br />
-                    <Button value="정보 수정" className="text-white bg-gray-400" />
+                    <Button
+                        value="정보 수정"
+                        onClick={onModify}
+                        className="text-white bg-gray-400"
+                    />
                 </Box>
             </div>
         </div>
