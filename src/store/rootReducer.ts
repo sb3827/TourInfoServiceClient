1d9ebaa3631a2d50d1af1d98cad5991f3274b954
yununyu <myamoly10@gmail.com>
2024-01-10 11:35:46 +0900

    사용하지 않는 slice 제거

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,15 +3,13 @@ import ReportSlice from './slices/ReportSlice'
 import LoginSlice from './slices/LoginSlice'
 import SearchSlice from './slices/SearchSlice'
 import ManagerSlice from './slices/ManagerSlice'
-import FindSlice from './slices/FindSlice'
 
 //리듀서 등록
 export const rootReducer = combineReducers({
     report: ReportSlice.reducer,
     login: LoginSlice.reducer,
     search: SearchSlice.reducer,
-    manager: ManagerSlice.reducer,
-    find: FindSlice.reducer
+    manager: ManagerSlice.reducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
