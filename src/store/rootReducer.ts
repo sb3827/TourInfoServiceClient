f27b3c6adc2e71d80cef1f76b20113313acf2b17
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-10 14:52:44 +0900

    dnd를 위한 리덕스 추가

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,13 +3,15 @@ import ReportSlice from './slices/ReportSlice'
 import LoginSlice from './slices/LoginSlice'
 import SearchSlice from './slices/SearchSlice'
 import ManagerSlice from './slices/ManagerSlice'
+import PlaceRegistSlice from './slices/PlaceSlice'
 
 //리듀서 등록
 export const rootReducer = combineReducers({
     report: ReportSlice.reducer,
     login: LoginSlice.reducer,
     search: SearchSlice.reducer,
-    manager: ManagerSlice.reducer
+    manager: ManagerSlice.reducer,
+    placRegist: PlaceRegistSlice.reducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
