81134abe73c1115eeb7e787adac64f6b6e5760c2
honghyibeom <ghdgmlqja1@naver.com>
2024-01-10 14:48:29 +0900

    팔로우 기능 api

--- src/api/UserSearch/UserSearch.tsx
+++ src/api/UserSearch/UserSearch.tsx
@@ -1,5 +1,6 @@
 import {UserSearchData} from '../../data/User/User'
-import {commonAxios} from '../Axios/CommonAxios'
+import {commonAxios } from '../Axios/CommonAxios'
+import {refreshAxios} from '../Axios/RefreshAxios'
 
 // 유저 검색 결과 불러오기
 export const getSearchUserInfo = async (
@@ -9,3 +10,28 @@ export const getSearchUserInfo = async (
         return response.data
     
 }
+
+// 유저 팔로우 기능
+export const postFollow = async (
+    memberMno:number,
+    followerMno:number
+    ): Promise<void> => {
+        const response = await refreshAxios.post(`/follow/following`, {
+            memberMno,
+            followerMno
+        })
+    return response.data    
+}
+
+// 유저 팔로우 취소 기능
+export const deleteFollow = async (
+    memberMno:number,
+    followerMno:number
+    ): Promise<void> => {
+        const response = await refreshAxios.post(`/follow/following`, {
+            memberMno,
+            followerMno
+        })
+    return response.data    
+}
+
