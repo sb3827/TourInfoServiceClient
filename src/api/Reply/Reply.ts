6bb05a4750c6c9eac58f670a246f3303fa8911b2
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-17 01:45:54 +0900

    댓글 등록 수정 삭제 추가

--- src/api/Reply/Reply.ts
+++ src/api/Reply/Reply.ts
@@ -1,5 +1,13 @@
-import {registReplyData, replyData} from '../../data/Reply/Reply'
+import {
+    deleteReplyData,
+    registReplyData,
+    replyData,
+    reportReplyData,
+    reportReplyResponseData,
+    updateReplyData
+} from '../../data/Reply/Reply'
 import {commonAxios} from '../Axios/CommonAxios'
+import {refreshAxios} from '../Axios/RefreshAxios'
 
 //댓글 들고오기
 export const getParentReply = async (bno: number): Promise<replyData[]> => {
@@ -17,6 +25,30 @@ export const getChildreply = async (bno: number, rno: number): Promise<replyData
 export const createReply = async (
     registReply: registReplyData
 ): Promise<{bno: number}> => {
-    const response = await commonAxios.post('/reply/regist', registReply)
+    const response = await refreshAxios.post('/reply/register', registReply)
+    return response.data
+}
+
+//댓글 수정
+export const updateReply = async (
+    updateReply: updateReplyData
+): Promise<{rno: number}> => {
+    const response = await refreshAxios.put('/reply/update', updateReply)
+    return response.data
+}
+
+//댓글 삭제
+export const deleteReply = async (
+    deleteReply: deleteReplyData
+): Promise<{rno: number}> => {
+    const response = await refreshAxios.put('/reply/delete', deleteReply)
+    return response.data
+}
+
+//댓글 신고
+export const reportReply = async (
+    reportReply: reportReplyData
+): Promise<reportReplyResponseData> => {
+    const response = await refreshAxios.post('/reply/report', reportReply)
     return response.data
 }
