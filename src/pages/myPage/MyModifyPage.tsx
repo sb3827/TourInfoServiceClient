12116af85fb99bfcdfeecfbcdd94acc1dea48e9a
이해창 <gockd9968@naver.com>
2024-01-17 16:43:38 +0900

    회원정보수정페이지 스타일 변경

--- src/pages/myPage/MyModifyPage.tsx
+++ src/pages/myPage/MyModifyPage.tsx
@@ -89,9 +89,9 @@ export const MyModifyPage = () => {
     }, [])
 
     return (
-        <div>
-            <div className="flex flex-row m-12">
-                <div className="items-center w-1/4">
+        <div className="flex items-center justify-center h-full ">
+            <div className="flex flex-row w-2/3 m-12">
+                <div className="flex flex-col items-center justify-center w-1/3">
                     <input
                         type="file"
                         className="hidden"
@@ -102,13 +102,22 @@ export const MyModifyPage = () => {
                     <img
                         src={ProfileImage}
                         alt="프로필사진"
-                        className="flex justify-center rounded-full cursor-pointer w-60"
+                        className="rounded-full cursor-pointer w-60 "
                         onClick={() => {
                             if (fileInput.current) {
                                 fileInput.current.click()
                             }
                         }}
                     />
+                    {/* <Button
+                        value="수정하기"
+                        onClick={() => {
+                            if (fileInput.current) {
+                                fileInput.current.click()
+                            }
+                        }}
+                        className="mt-8 w-28"
+                    /> */}
                 </div>
                 <div className="items-center w-2/3">
                     <div className="p-4">
@@ -151,6 +160,14 @@ export const MyModifyPage = () => {
                             onUserUpdate()
                             alert('회원정보를 수정하였습니다.')
                         }}
+                        className="w-28"
+                    />
+                    <Button
+                        value="취소"
+                        onClick={() => {
+                            // 다시 마이페이지로 이동
+                        }}
+                        className="w-28"
                     />
                 </div>
             </div>
