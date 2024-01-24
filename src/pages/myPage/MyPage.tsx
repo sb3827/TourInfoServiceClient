fac4c05d3382fb54016ccec92f72094e6e49f30c
이해창 <gockd9968@naver.com>
2024-01-24 10:55:09 +0900

    디자인 수정

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -5,15 +5,17 @@ export const MyPage = () => {
     const {mno} = useParams()
     return (
         <div>
-            <div className="flex flex-row">
-                <div className="w-1/2 mt-8 ml-32 mr-8 border rounded-xl">
-                    <ProfileBox mno={Number(mno)} />
-                </div>
-                <div className="w-1/2 mt-8 mr-32 border rounded-xl">
-                    <SimpleMap mno={Number(mno)} />
+            <div className="flex flex-row w-full ">
+                <div className="flex justify-center w-full">
+                    <div className="w-1/2 mt-8 ml-32 mr-8 border rounded-xl">
+                        <ProfileBox mno={Number(mno)} />
+                    </div>
+                    <div className="w-1/2 mt-8 mr-32 border rounded-xl">
+                        <SimpleMap mno={Number(mno)} />
+                    </div>
                 </div>
             </div>
-            <div className="flex justify-center w-full mt-4">
+            <div className="flex justify-center mt-4">
                 <WritingButton mno={Number(mno)} />
             </div>
         </div>
