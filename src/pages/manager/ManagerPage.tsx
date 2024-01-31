846d02d338a0bfd150ec5d546e835c03c2f8051f
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-01-31 12:28:54 +0900

    관리자 페이지 타이틀 추가

--- src/pages/manager/ManagerPage.tsx
+++ src/pages/manager/ManagerPage.tsx
@@ -1,10 +1,11 @@
-import {Box, FindBox, ReportBox, WaitBox} from '../../components/index'
+import {Box, FindBox, ReportBox, Title, WaitBox} from '../../components/index'
 
 //관리자 페이지
 
 export const Manager = () => {
     return (
         <Box>
+            <Title className="mb-5 text-darkGreen">관리자 페이지</Title>
             <FindBox />
             <WaitBox />
             <ReportBox />
