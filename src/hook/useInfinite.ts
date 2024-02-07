9c3c0a87208e6170ab1a1624cf3bc4d939f039ae
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-02-07 17:42:23 +0900

    폴더명 변경 테스트

--- src/hook/useInfinite.ts
+++ src/hook/useInfinite.ts
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react'
+import {useEffect, useRef} from 'react'
 
 export default function useInfinite(callback: () => void) {
     const target = useRef<HTMLDivElement | null>(null)
