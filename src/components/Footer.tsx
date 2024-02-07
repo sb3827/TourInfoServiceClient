baa5f7d149b4eb632bc89382e702795c62121a8b
sb3827 <mmk275@naver.com>
2024-02-07 20:24:52 +0900

    fix design of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import {FC} from 'react'
 import {Outlet} from 'react-router-dom'
+import Notion from '../assets/Notion.png'
+import GitHub from '../assets/GitHub.png'
 
 type FooterProps = {}
 
@@ -7,19 +9,51 @@ export const Footer: FC<FooterProps> = ({}) => {
     return (
         <div>
             <Outlet />
-            <footer className="items-center justify-center hidden w-full text-xs text-center text-gray-300 md:flex bg-slate-600">
-                <div className="flex-row items-center justify-center py-2">
-                    <p className="m-2">배낭 가져와 - 여행의 발견 (Discovery of Travel)</p>
-                    {/* 아래 수정해야함(깃허브 주소 추후 수정) */}
-                    <p className="m-2">
-                        Git Hub :&nbsp;
-                        <a
-                            href="https://github.com/sb3827/TourInfoServiceClient"
-                            className="text-gray-300 underline cursor-pointer hover:text-gray-300 hover:underline">
-                            https://github.com/sb3827/TourInfoServiceClient
-                        </a>
-                    </p>
-                    <p className="m-2">Copyright 2024 All &copy; rights reserved</p>
+            <footer className="hidden w-full text-gray-300 md:flex bg-slate-600">
+                <div className="flex flex-row justify-between w-screen mx-8 my-3">
+                    <div className="my-auto">
+                        <div className="text-left">
+                            <span className="text-2xl font-semibold">
+                                여행의 발견 (Discovery of Travel)
+                            </span>
+                            <p className="">of</p>
+                            <span className="">Team. 배낭 가져와</span>
+                        </div>
+                    </div>
+                    <div className="my-auto">
+                        <div className="flex flex-row justify-end my-2 text-xs">
+                            <a
+                                href="https://github.com/sb3827/TourInfoServiceClient"
+                                className="flex flex-col items-center justify-between mx-2">
+                                <img
+                                    src={GitHub}
+                                    className="w-6 bg-white rounded-full"
+                                    alt="react repository"></img>
+                                <p>react</p>
+                            </a>
+                            <a
+                                href="https://github.com/sb3827/TourInfoServiceServer"
+                                className="flex flex-col items-center justify-between mx-2">
+                                <img
+                                    src={GitHub}
+                                    className="w-6 bg-white rounded-full"
+                                    alt="spring repository"></img>
+                                <p>spring</p>
+                            </a>
+                            <a
+                                href="http://sangbaek.notion.site/DoT-Discovery-of-Travel-f6c356624ee441ff9cbe1bfbe6ea72a4?pvs=74"
+                                className="flex flex-col items-center justify-between ml-2">
+                                <img
+                                    src={Notion}
+                                    className="w-6 bg-white rounded-full"
+                                    alt="Notion"></img>
+                                <p>Notion</p>
+                            </a>
+                        </div>
+                        <p className="mt-2 text-xs">
+                            Copyright 2024 All &copy; rights reserved
+                        </p>
+                    </div>
                 </div>
             </footer>
         </div>
