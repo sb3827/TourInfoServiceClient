d84197c33684659c8bc503b866ed5f82a84b7092
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-02-07 18:00:46 +0900

    주석 추가

--- src/hook/useInfinite.ts
+++ src/hook/useInfinite.ts
@@ -1,5 +1,6 @@
 import {useEffect, useRef} from 'react'
 
+//아직 사용하지 않음 - 추후에 무한 스크롤 부분 해당 커스텀 훅으로 수정할 예정
 export default function useInfinite(callback: () => void) {
     const target = useRef<HTMLDivElement | null>(null)
 
