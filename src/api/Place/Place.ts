91cc81e86b18444502d1ae3646a6ba75863ca77e
sb3827 <mmk275@naver.com>
2024-02-08 09:46:17 +0900

    관리자 장소 삭제, 수정 추가

--- src/api/Place/Place.ts
+++ src/api/Place/Place.ts
@@ -1,8 +1,25 @@
+import {refreshAxios} from '../Axios/RefreshAxios'
 import {placeInfo} from './../../data/Place/Place'
 import {commonAxios} from './../Axios/CommonAxios'
 
 // 장소 등록
 export const registerPlace = async (place: placeInfo): Promise<void> => {
     const response = await commonAxios.post(`/place/register`, place)
+    return response.data
+}
+
+// 장소 삭제
+export const deletePlace = async (pno: number): Promise<void> => {
+    const response = await refreshAxios.delete(`/place/delete?pno=${pno}`)
+    return response.data
+}
+
+export type modiPlace = {
+    pno: number
+    title: string
+}
+// 장소명 수정
+export const modifyPlace = async (place: modiPlace): Promise<void> => {
+    const response = await refreshAxios.put(`/place/modify?pno=${place.pno}`, place.title)
     return response.data
 }
--- src/pages/PlaceDetails/PlaceDetails.tsx
+++ src/pages/PlaceDetails/PlaceDetails.tsx
@@ -1,16 +1,42 @@
-import {useState} from 'react'
-import {Box, SearchMap, Subtitle, BoardToggle, Title} from '../../components'
+import {useEffect, useMemo, useRef, useState} from 'react'
+import {
+    Box,
+    SearchMap,
+    Subtitle,
+    BoardToggle,
+    Title,
+    DropIcon,
+    Input,
+    Button
+} from '../../components'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {useNavigate, useParams} from 'react-router-dom'
-import {faList, faPlus} from '@fortawesome/free-solid-svg-icons'
+import {faEllipsisVertical, faList, faPlus} from '@fortawesome/free-solid-svg-icons'
 import {PlaceBoardData} from '../../data'
 import PlaceDetailsItem from '../../components/Place/PlaceDetailsItem'
+import {useSelector} from 'react-redux'
+import {RootState} from '../../store/rootReducer'
+import {deletePlace, modifyPlace} from '../../api'
 
 // 장소 상세 페이지
 export const PlaceDetails = () => {
+    const postText = ['수정', '신고', '삭제']
+    const user = useSelector((state: RootState) => state.login.mno)!
+    const role = useSelector((state: RootState) => state.login.role)
+    const [enables, setEnables] = useState<boolean[]>([false, true, false])
+    const [modifing, setModifing] = useState<boolean>(false)
+    const [report, setReport] = useState<boolean>(false)
     const [place, setPlace] = useState<PlaceBoardData | null>(null)
     const {pno} = useParams()
     const navigate = useNavigate()
+
+    const titleRef = useRef<HTMLInputElement | null>(null)
+
+    useEffect(() => {
+        if (role === 'ADMIN') {
+            setEnables([true, false, true])
+        }
+    }, [role])
 
     //게시글 작성으로 이동 -> pno pname 넘겨줘야함
     function onPlacePosting() {
@@ -27,12 +53,104 @@ export const PlaceDetails = () => {
         setPlace(placeData)
     }
 
+    function modifyTitle() {
+        // 제목 수정
+        setModifing(true)
+    }
+
+    function openModal() {
+        // report modal 열기
+        setReport(true)
+    }
+    function closeModal() {
+        // report modal 닫기
+        setReport(false)
+    }
+    function requestModify() {
+        // 제목 수정 요청
+        const title = titleRef.current?.value as string
+        if (title === '') {
+            alert('장소 명을 작성하세요')
+            return
+        }
+        try {
+            if (window.confirm('해당 장소를 수정하시겠습니까?')) {
+                modifyPlace({pno: place?.pno!, title: title})
+                setPlace(place => ({
+                    ...place!,
+                    name: title
+                }))
+                setModifing(false)
+            }
+        } catch (error) {
+            alert('수정 실패')
+        }
+    }
+    function cancel() {
+        setModifing(false)
+    }
+
+    function delPlace() {
+        // 장소 삭제
+        try {
+            if (window.confirm('해당 장소를 삭제하시겠습니까?')) {
+                deletePlace(place?.pno!)
+                navigate('/board/place')
+            }
+        } catch (error) {
+            alert('삭제 실패')
+        }
+    }
+
+    const titleText = useMemo(
+        () =>
+            place && place.name ? (
+                <Title className="xl:my-3 xl:py-3">{place.name}</Title>
+            ) : null,
+        [place]
+    )
+
     return (
         <Box>
             <div className="w-1/2">
                 <div className="flex justify-center w-full">
                     <div className="flex flex-col justify-around w-full">
-                        <Title className="xl:my-3 xl:py-3">{place && place.name}</Title>
+                        <div className="flex flex-row items-center justify-between">
+                            {!modifing && <div></div>}
+                            {modifing && (
+                                <Input
+                                    className="w-full"
+                                    placeholder="수정할 이름을 입력해 주세요"
+                                    ref={titleRef}></Input>
+                            )}
+                            {!modifing && titleText}
+                            {user && !modifing && (
+                                <DropIcon
+                                    itemTexts={postText}
+                                    itemActions={[modifyTitle, openModal, delPlace]}
+                                    itemEnabled={enables}>
+                                    <FontAwesomeIcon
+                                        className="ml-2 hover:cursor-pointer"
+                                        icon={faEllipsisVertical}
+                                        size="xl"
+                                    />
+                                </DropIcon>
+                            )}
+                            {modifing && (
+                                <Button
+                                    className="text-white bg-darkGreen"
+                                    value={'수정'}
+                                    onClick={requestModify}
+                                />
+                            )}
+                            {modifing && (
+                                <Button
+                                    className="btn-error"
+                                    value={'취소'}
+                                    onClick={cancel}
+                                />
+                            )}
+                        </div>
                         <div className="overflow-hidden shadow-xl rounded-xl h-5/6 xl:h-full xl:mb-10">
                             <SearchMap
                                 places={place && [place]}
@@ -66,6 +184,7 @@ export const PlaceDetails = () => {
                     <PlaceDetailsItem pno={Number(pno)} isAd={true} />
                 </BoardToggle>
             </div>
+            {report && <div>report modal 자리</div>}
         </Box>
     )
 }
